efd7770128e727a1193e16316c38171fa11960d7
Mick Phillips <mick.phillips@gmail.com>
2014-06-18 18:17:39 +0100

    Created a config submodule for cockpit.

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import os
+from ConfigParser import ConfigParser
+
+_path = __path__[0]
+_files = [os.path.sep.join([_path, file])
+            for file in os.listdir(_path) if file.endswith('.conf')]
+
+try:
+    _files.append(_files.pop(_files.index(
+        os.path.sep.join([_path,'master.conf']))))
+    print _files
+except:
+    pass
+
+print _files
+config = ConfigParser()
+print config.read(_files)
