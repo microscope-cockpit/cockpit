279101350ecd6f6aeff543d474d7ab4e1f745167
Mick Phillips <mick.phillips@gmail.com>
2014-06-23 17:27:34 +0100

    Added to sr470 __init__ .

--- devices/sr470.py
+++ devices/sr470.py
@@ -13,10 +13,22 @@ import util.logger
 import decimal
 import re
 import telnetlib
+from config import config
 
 CLASS_NAME = 'StanfordShutterDevice'
+CONFIG_NAME = 'sr470'
 
 class StanfordShutterDevice(device.Device):
-	def __init__(self):
-		device.Device.__init__(self)
-		self.isActive = True
+    def __init__(self):
+        device.Device.__init__(self)
+        self.isActive = config.has_section(CONFIG_NAME)
+        self.ipAddress = config.get(CONFIG_NAME, 'ipAddress')
+        self.port = config.get(CONFIG_NAME, 'port')
+        self.controlledLightNames = set(
+            config.get(CONFIG_NAME, 'lights').split(';'))
+        if self.isActive:
+            events.subscribe('prepare for experiment', 
+                            self.prepareForExperiment)
+            events.subscribe('experiment complete', 
+                            self.cleanupAfterExperiment)
+        
