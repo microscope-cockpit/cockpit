202772cfb0af476399109897f9ce07b9d28d35f5
Mick Phillips <mick.phillips@gmail.com>
2014-09-08 16:27:42 +0100

    Configurator now takes values from config module.

--- devices/configurator.py
+++ devices/configurator.py
@@ -6,11 +6,17 @@ import handlers.configuratorHandler
 import os
 
 CLASS_NAME = 'ConfiguratorDevice'
+CONFIG_NAME = 'base'
+
+from config import config
+
 
 class ConfiguratorDevice(device.Device):
     def getHandlers(self):
-        root = os.path.expanduser('~')
-        config = {
+        root = config.get(CONFIG_NAME, 'root', 'C:' + os.path.sep)
+ 
+        # Default values.
+        configdict = {
                 'slideAltitude': 7370,
                 'slideTouchdownAltitude': 7900,
                 'dishAltitude': 5750,
@@ -18,7 +24,20 @@ class ConfiguratorDevice(device.Device):
                 'logDirectory': os.path.join(root, 'AA_MUI_LOGS'),
                 'configDirectory': os.path.join(root, 'AA_MUI_CONFIG'),
         }
+
+
+        # Update the configdict with values from config module.
+        if config.has_section(CONFIG_NAME):
+            for opt in config.options(CONFIG_NAME):
+                if opt in configdict:
+                    # Already exists. Make sure we keep the same type.
+                    dtype = type(config.get(opt))
+                    configdict.update({opt: type(config.get(CONFIG_NAME, opt))})
+                else:
+                    configdict.update({opt: config.get(CONFIG_NAME, opt)})
+
+
         return [handlers.configuratorHandler.ConfiguratorHandler(
-            'configuration', 'miscellaneous', {}, config)
+            'configuration', 'miscellaneous', {}, configdict)
         ]
 
