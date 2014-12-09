e62e310cf8e955cbed72b7a9ab449a621f1a0ea5
Mick Phillips <mick.phillips@gmail.com>
2014-12-09 11:28:03 +0000

    SR470 now configured during initialize.

--- devices/sr470.py
+++ devices/sr470.py
@@ -1,7 +1,11 @@
 """ Cockpit device for SRS SR470 shutter controller.
 
 Mick Phillips, 2014.
-Largely derived from Chris' delayGen device.
+
+This is a really simple driver for the SR470 shutter controller.  For now, all
+it does is make the SR470 respond to an external trigger, although it is
+feasible that the shutter controller could be programmed to control exposure
+timing.
 """
 
 import depot
@@ -32,14 +36,6 @@ class StanfordShutterDevice(device.Device):
                             self.onPrepareForExperiment)
             events.subscribe('experiment complete', 
                             self.onCleanupAfterExperiment)
-        ## Set of handlers we control.
-        self.handlers = set()
-        ## Our ExperimentExecutor handler.
-        self.executor = None
-        ## Cached exposure times we have set.
-        self.nameToExposureTime = {}
-        ## Cached trigger response delay
-        self.curDelay = None
 
 
     def initialize(self):
@@ -47,23 +43,33 @@ class StanfordShutterDevice(device.Device):
         self.connection.read_until("SR470 Telnet Session:")
         # Read out any trailing whitespace, e.g. newlines.
         self.connection.read_eager()
+        self.enableTrigger()
+
+
+    def enableTrigger(self):
+        # Set the shutter to 'normally closed'.
+        self.send("POLR 1")
+        # Set the control mode to external level. The shutter will be open as
+        # long as the external input is high.
+        self.send("SRCE 2")
 
 
     def onPrepareForExperiment(self, experiment):
         if config.has_section('dsp'):
             ## Rely on the DSP for timing: use external level control.
             # DSP trigger line is configured elsewhere.
-            self.connection.write("SRCE 2")
+            self.enableTrigger()
         else:
             ## We could use the SR470 controller for exposure timing here.
             pass
 
 
     def onCleanupAfterExperiment(self):
-        ## Assert shutter closed (also reverts to internal triggering).
-        self.connection.write("ASRT 0")
+        # Assert shutter closed (also reverts to internal triggering).
+        self.send("ASRT 0")
+        self.enableTrigger()
 
 
-    def sendCommand(self, command):
+    def send(self, command):
         self.connection.write(command + '\n')
         return self.connection.read_until('\n', 0.05)
