5c1cc3fb995429e106ad2e868dba23c64b1afda2
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2015-02-11 15:20:57 +0000

    updates to config files from running code base

--- config/cameras.py
+++ config/cameras.py
@@ -1,6 +1,7 @@
 ## _cameras [(label, dsp line, ip address, model)]
 cameras = [
-    ('West', 9146, 1<<0, '127.0.0.1', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585]),
-    ('East', 9145, 1<<1, '127.0.0.1', 7776, 'ixon', ['Cy5', 'FITC'], [670, 518]),
+    ('West', 7197, 1<<0, '172.16.0.20', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585],[0,0,1]),
     ]
-camera_keys = ['label', 'serial', 'line', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
+camera_keys = ['label', 'serial', 'line', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths','transform']
+
+#transform order [0]= rot90, [1]=flip_h, [2]=flip_v
