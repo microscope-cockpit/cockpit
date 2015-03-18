0617cb9f4a2313cf00eb68e4fec09dd698942142
Eric Branlund <eric@msg.ucsf.edu>
2015-03-18 10:40:56 -0700

    Applied Chris Weisiger's change from 2014/03/28: "Fixed an inverted boolean statment.".

--- devices-ucsfOrion/tirfMirror.py
+++ devices-ucsfOrion/tirfMirror.py
@@ -166,7 +166,7 @@ class TIRFMirrorDevice(device.Device):
         if (not isEnabled) or 'LED' in handler.name:
             return
         # EPI doesn't use TIRF; everyone else does.
-        self.activeButton.setActive('EPI' in handler.name)
+        self.activeButton.setActive('EPI' not in handler.name)
         # Skip if we have multiple active light sources.
         allLights = depot.getHandlersOfType(depot.LIGHT_TOGGLE)
         numActive = sum([(l.getIsEnabled() and 'LED' not in handler.name) for l in allLights])
