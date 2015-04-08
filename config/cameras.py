6e3a9722ec6d3a682ce45c78c346126476170bcf
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2015-04-08 16:03:55 +0100

    Micks dome config files

--- config/cameras.py
+++ config/cameras.py
@@ -1,5 +1,6 @@
 cameras = [
-    ('West', 9146, 1<<0, '127.0.0.1', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585]),
-    ('East', 9145, 1<<1, '127.0.0.1', 7776, 'ixon', ['Cy5', 'FITC'], [670, 518]),
+    ('West', 9146, 1<<8, '192.168.1.2', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585], (0,0,1)),
+    ('East', 9145, 1<<9, '192.168.1.2', 7778, 'ixon', ['Cy5', 'FITC'], [670, 518], (0,0,1)),
     ]
-camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
+camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths','baseTransform']
+#transform order [0]=rot90,[1]=flip_h,[2]=flip_v
