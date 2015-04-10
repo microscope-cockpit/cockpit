3f236a215bf081db068370d69d72ba440e79e36e
Mick Phillips <mick.phillips@gmail.com>
2015-04-10 20:42:51 +0100

    First commit of devices/linkamStage .

---
+++ devices/linkamStage.py
@@ -0,0 +1,162 @@
+import device
+import events
+import gui.guiUtils
+import handlers.stagePositioner
+import interfaces
+import Pyro4
+import threading
+import util.logger as logger
+import util.threads
+import util.userConfig
+
+import time
+import wx
+import re # to get regular expression parsing for config file
+
+from config import config
+CLASS_NAME = 'CockpitLinkamStage'
+
+CONFIG_NAME = 'linkam'
+LIMITS_PAT = r"(?P<limits>\(\s*\(\s*[-]?\d*\s*,\s*[-]?\d*\s*\)\s*,\s*\(\s*[-]?\d*\s*\,\s*[-]?\d*\s*\)\))"
+DEFAULT_LIMITS = ((0, 0), (5000, 3000))
+
+class CockpitLinkamStage(device.Device):
+    def __init__(self):
+        device.Device.__init__(self)
+        ## Connection to the XY stage controller (serial.Serial instance).
+        self.remote = None
+        ## Lock around sending commands to the XY stage controller.
+        self.xyLock = threading.Lock()
+        ## Cached copy of the stage's position.
+        self.positionCache = (None, None)
+        ## Target positions for movement in X and Y.
+        self.motionTargets = [None, None]
+        ## Time of last action using the stage.
+        self.lastPiezoTime = time.time()
+        ## Stage velocity
+        self.stageVelocity = [None, None]
+
+        self.isActive = config.has_section(CONFIG_NAME)
+        if self.isActive:
+            self.ipAddress = config.get(CONFIG_NAME, 'ipAddress')
+            self.port = int(config.get(CONFIG_NAME, 'port'))
+            try :
+                limitString = config.get(CONFIG_NAME, 'softlimits')
+                parsed = re.search(LIMITS_PAT, limitString)
+                if not parsed:
+                    # Could not parse config entry.
+                    raise Exception('Bad config: PhysikInstrumentsM687 Limits.')
+                    # No transform tuple
+                else:    
+                    lstr = parsed.groupdict()['limits']
+                    self.softlimits=eval(lstr)
+            except:
+                logger.log.warn('Could not parse limits from config: using defaults.')
+                print "No softlimits section setting default limits"
+                self.softlimits = ((0, 0), (7000, 3000))
+            events.subscribe('user logout', self.onLogout)
+            events.subscribe('user abort', self.onAbort)
+
+
+    def initialize(self):
+        uri = "PYRO:%s@%s:%d" % (CONFIG_NAME, self.ipAddress, self.port)
+        self.remote = Pyro4.Proxy(uri)
+        self.remote.connect()
+        self.getPosition(shouldUseCache = False)
+        
+
+    def homeMotors(self):
+        """Home the motors."""
+        self.remote.homeMotors()
+        self.sendPositionUpdates()
+
+
+    def onLogout(self, *args):
+        pass
+        
+
+    def onAbort(self, *args):
+        """Stop motion immediately."""
+        pass
+
+
+    def getHandlers(self):
+        result = []
+        # zip(*limits) transforms ((x0,y0),(x1,y1)) to ((x0,x1),(y0,y1))
+        for axis, (minPos, maxPos) in enumerate(zip(*self.softlimits)):
+            result.append(
+                handlers.stagePositioner.PositionerHandler(
+                    "%d linkam mover" % axis, "%d stage motion" % axis, False,
+                    {'moveAbsolute': self.moveAbsolute,
+                         'moveRelative': self.moveRelative,
+                         'getPosition': self.getPosition,
+                         'setSafety': self.setSafety},
+                    axis,
+                    [1, 2, 5, 10, 50, 100, 200], # step sizes
+                    3, # initial step size index,
+                    (minPos, maxPos), # soft limits
+                    (minPos, maxPos) # hard limits
+                    )
+                )
+        return result
+
+
+    def moveAbsolute(self, axis, pos):
+        """Move a stage axis to new position, pos."""
+        pos = int(pos)
+        if axis == 0:
+            newPos = (pos, None)
+        elif axis == 1:
+            newPos = (None, pos)
+        with self.xyLock:
+            # moveToXY(x, y), where None indicates no change.
+            self.remote.moveToXY(*newPos)
+        self.motionTargets[axis] = pos
+        self.sendPositionUpdates()
+
+
+    def moveRelative(self, axis, delta):
+        if delta:
+            curPos = self.positionCache[axis]
+            self.moveAbsolute(axis, curPos + delta)
+
+
+    @util.threads.callInNewThread
+    def sendPositionUpdates(self):
+        """Send XY stage positions until it stops moving."""
+        while self.remote.isMoving():
+            coords = self.getPosition(shouldUseCache=False)
+            for axis, value in enumerate(coords):
+                events.publish('stage mover',
+                               '%d linkam mover' % axis, 
+                               axis, value)
+            curPosition = coords
+            time.sleep(.1)
+        
+        for axis in (0, 1):
+            events.publish('stage stopped', '%d linkam mover' % axis)
+            self.motionTargets = [None, None]
+        return
+
+    ## Get the position of the specified axis, or both axes by default.
+    def getPosition(self, axis=None, shouldUseCache=True):
+        """Return the position of one or both axes.
+
+        If axis is None, return positions of both axes.
+        Query the hardware if shouldUseCache is False.
+        """
+        if not shouldUseCache:
+            position = self.remote.getPosition()
+            self.positionCache = position
+        if axis is None:
+            return self.positionCache
+        else:
+            return self.positionCache[axis]
+
+
+    def setSafety(self, axis, value, isMax):
+        pass
+
+
+    def makeInitialPublications(self):
+        self.sendPositionUpdates()
