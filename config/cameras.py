3f44bdf61dadceec7d82629f87a42f1b535da193
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2015-04-14 17:28:33 +0100

    Swapped cameras between east and west as they were wrong

--- config/cameras.py
+++ config/cameras.py
@@ -1,6 +1,6 @@
 cameras = [
-    ('West', 9146, 1<<8, '192.168.1.2', 7777, 'ixon', ['GFP'], [525], (0,0,1)),
-    ('East', 9145, 1<<9, '192.168.1.2', 7778, 'ixon', ['TRITC'], [600], (0,0,1)),
+    ('West', 9146, 1<<8, '192.168.1.2', 7777, 'ixon', ['TRITC'], [525], (0,0,1)),
+    ('East', 9145, 1<<9, '192.168.1.2', 7778, 'ixon', ['GFP'], [600], (0,0,1)),
     ]
 camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths','baseTransform']
 #transform order [0]=rot90,[1]=flip_h,[2]=flip_v
