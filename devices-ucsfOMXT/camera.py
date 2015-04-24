e530cbd5e526afdcc5176e3c703a9f18e307a230
Eric Branlund <eric@msg.ucsf.edu>
2015-04-23 22:13:46 -0700

    Removed stray print and return statment from CameraManager.performSubscriptions().

--- devices-ucsfOMXT/camera.py
+++ devices-ucsfOMXT/camera.py
@@ -100,7 +100,3 @@ class CameraManager(device.Device):
     def performSubscriptions(self):
         for camera in self.cameras:
             camera.performSubscriptions()
-
-
-        print "camera.CameraManager.makeUI"
-        return None
