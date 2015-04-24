10331fb77443d87d2ca475bd93dfd4e3b1480ecc
Mick Phillips <mick.phillips@gmail.com>
2015-04-24 14:09:49 +0100

    Moved showKeys into class. Handles window resizing better. Replaced legend with labels. Changed tick labels.

--- gui/valueLogger.py
+++ gui/valueLogger.py
@@ -22,6 +22,7 @@ import events
 import interfaces.valueLogger
 import gui.guiUtils
 import matplotlib
+import matplotlib.dates
 matplotlib.use('WXAgg')
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.backends.backend_wx import NavigationToolbar2Wx
@@ -33,11 +34,14 @@ import wx
 
 
 class ValueLoggerWindow(wx.Frame):
+    """The main ValueLogger window."""
     def __init__(self, parent, title='value logger'):
         super(ValueLoggerWindow, self).__init__(
                 parent, -1, title,
                 style = wx.CAPTION | wx.RESIZE_BORDER)
+        # Bind to close event.
         self.Bind(wx.EVT_CLOSE, self.onClose)
+        # Create our panel.
         self.panel = ValueLoggerPanel(self)
         self.Show()
 
@@ -49,26 +53,70 @@ class ValueLoggerWindow(wx.Frame):
 
 
 class ValueLoggerPanel(wx.Panel):
+    """A panel for ValueLoggerWindow."""
     def __init__(self, parent):
         super(ValueLoggerPanel, self).__init__(parent)
+        ## A mapping of names to booleans.
+        self.showKeys = {}
+        ## A mapping of name to line objects.
         self.lines = {}
+        ## A mapping of name to labels.
+        self.labels = {}
+        ## matplotlib objects
         self.figure = Figure()
         self.axes = self.figure.add_subplot(111)
-        self.axes.set_position([0.1, 0.1, 0.6, 0.85])
         self.canvas = FigureCanvas(self, -1, self.figure)
+        ## Border around axes in pixels (left, bottom, right, top)
+        self.border = (56, 88, 192, 24)
+        ## Main panel sizer.
         self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
-        self.SetSizerAndFit(self.sizer)
-        events.subscribe("valuelogger update", self.draw)
-        self.canvas.Bind(wx.EVT_RIGHT_DOWN, self.onRightMouse)
 
+        # Add the canvas to the sizer.
+        self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
+        self.SetSizer(self.sizer)
+        self.sizer.SetSizeHints(parent)
+        # Redraw when new logged values are available.
+        events.subscribe("valuelogger update", self.draw)
+        # Show a menu of available data on right mouse click.
+        self.canvas.Bind(wx.EVT_RIGHT_DOWN, self.onRightMouse)
+        # Bind to our resize handler.
+        self.Bind(wx.EVT_SIZE, self.onResize)
+
+        ## Set axis properties.
+        self.axes.xaxis_date()
+        self.axes.xaxis.set_major_formatter(
+                matplotlib.dates.DateFormatter('%H:%M:%S'))
+        self.axes.xaxis.set_major_locator(
+                matplotlib.ticker.LinearLocator() )
+
+        ## Set intial figure borders.
+        self.setFigureBorder(self.Size)
+        
+
+    def setFigureBorder(self, size):
+        """Set the figure border from the window size."""
+        border = self.border # (left, bottom, right, top)
+        
+        pos = (float(self.border[0]) / size[0],
+               float(self.border[1]) / size[1],
+               1 - float(self.border[2] + self.border[0]) / size[0],
+               1 - float(self.border[3] + self.border[1]) / size[1])
+        self.axes.set_position(pos)
+
+
+    def onResize(self, event):
+        """Resize event handler."""
+        self.setFigureBorder(event.Size)
+        event.Skip()
+        
 
     def onRightMouse(self, *args):
+        """Show a menu of available data on right mouse click."""
         menu = wx.Menu()
-        for i, (key, enabled) in enumerate(sorted(showKeys.items())):
+        for i, (key, enabled) in enumerate(sorted(self.showKeys.items())):
             menu.Append(i, key, '', wx.ITEM_CHECK)
             menu.Check(i, enabled)
-            wx.EVT_MENU(menu, i, lambda event, k=key:toggleShowKey(k))
+            wx.EVT_MENU(menu, i, lambda event, k=key:self.toggleShowKey(k))
         gui.guiUtils.placeMenuAtMouse(self, menu)
 
     
@@ -80,40 +128,68 @@ class ValueLoggerPanel(wx.Panel):
 
         dataTimes = interfaces.valueLogger.instance.times
         dataSeries = interfaces.valueLogger.instance.series
-
         for (key, series) in dataSeries.iteritems():
             if key == 'time':
+                # Don't plot time vs time.
                 continue
-            if not showKeys:
-                showKeys[key] = True
-            elif key not in showKeys:
-                showKeys[key] = False
+            if not self.showKeys:
+                # If showKeys is empty, create one True entry.
+                self.showKeys[key] = True
+            elif key not in self.showKeys:
+                # Require explicit instruction to show other items.
+                self.showKeys[key] = False
 
             line = self.lines.get(key, None)
-            if showKeys[key]:
+            if self.showKeys[key]:
+                # If we are to show this line ...
                 if line:
+                    # if the line exists, update the data
                     line.set_data(dataTimes, series)
+                    # Make the series label track the most recent value.
+                    self.labels[key].set_y(series[-1])
                 elif len(series) > 0:
-                    self.lines[key], = self.axes.plot(dataTimes, series)
+                    # otherwise, create the line if there is data available.
+                    self.lines[key], = self.axes.plot(
+                            dataTimes, 
+                            series)
+                    self.labels[key] = self.axes.text(dataTimes[-1], 
+                                                      series[-1], 
+                                                      ' %s' % key)
             else:
+                # We don't want to show this line.
                 if line:
+                    # If it exists, remove it from the graph.
                     self.axes.lines.remove(line)
                     self.lines[key] = None
-        self.axes.legend([k for k in showKeys if showKeys[k]], 
-                         loc='center left', bbox_to_anchor=(1, 0.5),
-                         prop={'size':11})
+        # Update the axes so that the current data sets fit.
         self.axes.relim()
         self.axes.autoscale_view()
+        # Rotate each tick mark
+        xaxis = self.axes.xaxis
+        ticksMajor = xaxis.get_ticklabels()
+        ticksMinor = xaxis.get_ticklabels(minor=True)
+        for tick in ticksMajor + ticksMinor:
+            tick.set_rotation(90)
+        # Put each series label at the far right of the axes.
+        xMax = xaxis.get_view_interval()[1]
+        for label in self.labels.values():
+            label.set_x(xMax)
+
+        # Update the canvas.
         self.canvas.draw()
         self.canvas.flush_events()
+
+        
+    def toggleShowKey(self, key):
+        """Toggle the state of an entry in showKeys."""
+        if self.showKeys[key] and self.showKeys.values().count(True) == 1:
+            # This is the only key set to True. Do not change it.
+            return
+        self.showKeys[key] = not self.showKeys[key]
         
 
-def toggleShowKey(key):
-    showKeys[key] = not showKeys[key]
-
-
 def makeWindow(parent):
+    """Create the ValueLogger window instance and associated objects."""
+    # The ValueLoggerWindow instance.
     global window
-    global showKeys
     window = ValueLoggerWindow(parent)
-    showKeys = {}
