a71a0c4c246f5da0539f135f6cd9a304b5f79ba7
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2015-05-07 22:03:33 +0100

    adjust histogram size to secondary motion limits or to default

--- gui/macroStage/macroStageZ.py
+++ gui/macroStage/macroStageZ.py
@@ -260,8 +260,15 @@ class MacroStageZ(macroStageBase.MacroStageBase):
             motorPos = self.curStagePosition[2]
             majorPos = interfaces.stageMover.getAllPositions()[0][2]
             minorLimits = interfaces.stageMover.getIndividualSoftLimits(2)
-            zMin = majorPos
-            zMax = majorPos
+             # Add the max range of motion of the first fine-motion controller.
+             #And subtract the lower limit
+             if len(minorLimits) > 1:
+                 minorPos= interfaces.stageMover.getAllPositions()[1][2]
+                 zMax = majorPos + minorLimits[1][1]
+                 zMin = majorPos -(minorPos-minorLimits[1][0])
+             else:
+                 zMax = majorPos +(SECONDARY_HISTOGRAM_SIZE/2.0)
+                 zMax = majorPos -(SECONDARY_HISTOGRAM_SIZE/2.0)
             # Add the max range of motion of the first fine-motion controller.
             if len(minorLimits) > 1:
                 zMax = majorPos + minorLimits[1][1]
