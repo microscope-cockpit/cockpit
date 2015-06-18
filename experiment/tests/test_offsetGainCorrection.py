77318de87c3d87c4c60dac32e7233253f99bcf98
tomparks <thomasparks@outlook.com>
2015-06-18 09:24:45 +0100

    Framework for offsetGainCorrection tests.

---
+++ experiment/tests/test_offsetGainCorrection.py
@@ -0,0 +1,64 @@
+import unittest
+
+class TestOffsetGainCorrectionExperiment(unittest.TestCase):
+    def test___init__(self):
+        # offset_gain_correction_experiment = OffsetGainCorrectionExperiment(cameras, lights, exposureSettings, numExposures, savePath, exposureMultiplier, maxIntensity, cosmicRayThreshold, numCollections, shouldPreserveIntermediaryFiles, **kwargs)
+        assert False # TODO: implement your test here
+
+    def test_generateActions(self):
+        # offset_gain_correction_experiment = OffsetGainCorrectionExperiment(cameras, lights, exposureSettings, numExposures, savePath, exposureMultiplier, maxIntensity, cosmicRayThreshold, numCollections, shouldPreserveIntermediaryFiles, **kwargs)
+        # self.assertEqual(expected, offset_gain_correction_experiment.generateActions(multiplier, activeCameras))
+        assert False # TODO: implement your test here
+
+    def test_makeFit(self):
+        # offset_gain_correction_experiment = OffsetGainCorrectionExperiment(cameras, lights, exposureSettings, numExposures, savePath, exposureMultiplier, maxIntensity, cosmicRayThreshold, numCollections, shouldPreserveIntermediaryFiles, **kwargs)
+        # self.assertEqual(expected, offset_gain_correction_experiment.makeFit(images))
+        assert False # TODO: implement your test here
+
+    def test_processImages(self):
+        # offset_gain_correction_experiment = OffsetGainCorrectionExperiment(cameras, lights, exposureSettings, numExposures, savePath, exposureMultiplier, maxIntensity, cosmicRayThreshold, numCollections, shouldPreserveIntermediaryFiles, **kwargs)
+        # self.assertEqual(expected, offset_gain_correction_experiment.processImages(multiplier))
+        assert False # TODO: implement your test here
+
+    def test_recordImage(self):
+        # offset_gain_correction_experiment = OffsetGainCorrectionExperiment(cameras, lights, exposureSettings, numExposures, savePath, exposureMultiplier, maxIntensity, cosmicRayThreshold, numCollections, shouldPreserveIntermediaryFiles, **kwargs)
+        # self.assertEqual(expected, offset_gain_correction_experiment.recordImage(image, camera))
+        assert False # TODO: implement your test here
+
+    def test_run(self):
+        # offset_gain_correction_experiment = OffsetGainCorrectionExperiment(cameras, lights, exposureSettings, numExposures, savePath, exposureMultiplier, maxIntensity, cosmicRayThreshold, numCollections, shouldPreserveIntermediaryFiles, **kwargs)
+        # self.assertEqual(expected, offset_gain_correction_experiment.run())
+        assert False # TODO: implement your test here
+
+    def test_waiter(self):
+        # offset_gain_correction_experiment = OffsetGainCorrectionExperiment(cameras, lights, exposureSettings, numExposures, savePath, exposureMultiplier, maxIntensity, cosmicRayThreshold, numCollections, shouldPreserveIntermediaryFiles, **kwargs)
+        # self.assertEqual(expected, offset_gain_correction_experiment.waiter())
+        assert False # TODO: implement your test here
+
+class TestExperimentUI(unittest.TestCase):
+    def test___init__(self):
+        # experiment_u_i = ExperimentUI(parent, configKey)
+        assert False # TODO: implement your test here
+
+    def test_augmentParams(self):
+        # experiment_u_i = ExperimentUI(parent, configKey)
+        # self.assertEqual(expected, experiment_u_i.augmentParams(params))
+        assert False # TODO: implement your test here
+
+    def test_getSettingsDict(self):
+        # experiment_u_i = ExperimentUI(parent, configKey)
+        # self.assertEqual(expected, experiment_u_i.getSettingsDict())
+        assert False # TODO: implement your test here
+
+    def test_loadSettings(self):
+        # experiment_u_i = ExperimentUI(parent, configKey)
+        # self.assertEqual(expected, experiment_u_i.loadSettings())
+        assert False # TODO: implement your test here
+
+    def test_saveSettings(self):
+        # experiment_u_i = ExperimentUI(parent, configKey)
+        # self.assertEqual(expected, experiment_u_i.saveSettings(settings))
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
