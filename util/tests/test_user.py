76dedd62ab95d6dbc8883194fe97dfad30afe979
tomparks <thomasparks@outlook.com>
2015-06-24 16:44:26 +0100

    Added user modification and logout tests.

--- util/tests/test_user.py
+++ util/tests/test_user.py
@@ -6,6 +6,8 @@ mockable by overriding os?
 import unittest
 import imp
 import mock
+import os
+import wx
 from mockManagers import mock_import
 
 
@@ -51,3 +53,48 @@ class test_user(unittest.TestCase):
         self.user.login(None)
 
         self.mocks['util.logger'].changeFile.assert_called_with('user')
+
+
+    def test_logout_publishes_logout(self):
+        '''Tests that when the user logs out, the event is published.'''
+        self.user.logout(False)
+        self.mocks['events'].publish.assert_called_with("user logout")
+
+
+    def test_logout_calls_login(self):
+        '''If login again, then logout should call login with the parent window.
+        '''
+        self.user.login = mock.Mock(name='loginMock')
+        self.user.logout()
+        self.assertEqual(type(self.user.login.call_args[0]), type(wx.Window))
+
+
+class test_user_modification(unittest.TestCase):
+
+    def setUp(self):
+        with mock_import(['events', 'files', 'gui', 'gui.loggingWindow',
+                          'gui.mainWindow', 'gui.mosaic', 'gui.mosaic.window',
+                          'util.logger', 'util.userConfig', 'wx', 'os']) as mocks:
+            import util.user
+            imp.reload(util.user)
+            self.mocks = mocks
+            # leave join available, as it is used to provide input to functions
+            # we do care about mocking
+            mocks['os'].path.join = os.path.join
+        self.user = util.user
+
+
+    def test_createUser(self):
+        '''createUser should create a folder in util.files.getDataDir()'''
+        self.user.files = mock.Mock()
+        self.user.files.getDataDir.return_value = 'pathto'
+        self.user.createUser('user')
+        self.mocks['os'].mkdir.assert_called_with('pathto/user')
+
+
+    def test_deleteUser(self):
+        '''createUser should remove a folder in util.files.getDataDir()'''
+        self.user.files = mock.Mock()
+        self.user.files.getDataDir.return_value = 'pathto'
+        self.user.deleteUser('user')
+        self.mocks['os'].rmdir.assert_called_with('pathto/user')
