3ac1cad6ccce3c249c4264782dcf535f3f750f15
Mick Phillips <mick.phillips@gmail.com>
2015-07-23 17:18:01 +0100

    Added PR startup values.

--- config/analog.py
+++ config/analog.py
@@ -11,13 +11,14 @@ aout_keys = ['name',
 aouts = [(
             'polrot',   # polarisation rotatoar
             'SI angle',    # moves angle 
-            2,          # on analogue out 2
+            1,          # on analogue out 2
             1,          # 1V per V
             (0, 10),
-            (None, None),
-            [0.01, 0.05, 0.1, 0.5, 1],
-            2,
-            None
+            (0, 10),
+            #[1.11, 1.32, 0.96], # actual values we want
+            [0, 0.22, -0.15], # values less startup_value, which the dumb DSP insists must be > 0.16
+            0,
+            1.11,
          ),
          (
             'z_insert',
@@ -28,6 +29,6 @@ aouts = [(
             (0, 91.13),
             [.05, .1, .5, 1, 2, 5, 10],
             2,
-            45,
+            100,
          )  
         ]
