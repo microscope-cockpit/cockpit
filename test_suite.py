76719f1e4b1ba90f5c2f1e59ca11d9263cf83f84
Thomas Parks <thomasparks@outlook.com>
2015-08-20 13:25:31 +0100

    Enabled all of the functional tests.

--- test_suite.py
+++ test_suite.py
@@ -16,30 +16,33 @@ thomasparks@outlook.com
 '''
 
 import unittest
-#from util.tests.test_logger import *
+from util.tests.test_logger import *
 
-#from util.tests.test_user import test_user, test_user_modification
+from util.tests.test_user import test_user, test_user_modification
 
-#from util.tests.test_importer import testImporter
+from util.tests.test_importer import testImporter
 
-#from util.tests.test_listener import testListener
-#from util.tests.test_connection import TestConnection
-#from util.tests.test_correctNonLinear import TestCorrector
+from util.tests.test_listener import testListener
+from util.tests.test_connection import TestConnection
+from util.tests.test_correctNonLinear import TestCorrector # prints everywere
 
 #from experiment.tests.test_full_chain import TestChain
 
-#from experiment.tests.test_experiment import TestExperiment
+from experiment.tests.test_experiment import TestExperiment # huge mess with threads
 
-#from experiment.tests.test_actionTable import TestActionTable
+from experiment.tests.test_actionTable import TestActionTable
 
 #from experiment.tests.test_dataSaver import TestDataSaver
 
-#from util.tests.test_colors import TestWavelengthToColor, TestHsvToRgb
+from util.tests.test_colors import TestWavelengthToColor, TestHsvToRgb
 
 #from util.tests.test_datadoc import TestDataDoc
 
 from handlers.tests.test_camera import testCamera
 from handlers.tests.test_drawer import testDrawer
 from handlers.tests.test_lightSource import TestLightSourceHandler
+
+from devices.tests.test_dsp import TestDSP
+
 if __name__ == '__main__':
     unittest.main()
