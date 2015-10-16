481530224363b67d9256001968881adc58220cd9
Mick Phillips <mick.phillips@gmail.com>
2015-10-16 11:32:52 +0100

    Added drawCrosshair().

--- gui/imageViewer/viewCanvas.py
+++ gui/imageViewer/viewCanvas.py
@@ -349,6 +349,16 @@ class ViewCanvas(wx.glcanvas.GLCanvas):
             self.shouldDraw = False
 
 
+    def drawCrosshair(self):
+        glColor3f(0, 255, 255)
+        glBegin(GL_LINES)
+        glVertex2f(0, HISTOGRAM_HEIGHT + 0.5 * (self.h - HISTOGRAM_HEIGHT) )
+        glVertex2f(self.w, HISTOGRAM_HEIGHT + 0.5 * (self.h - HISTOGRAM_HEIGHT) )
+        glVertex2f(0.5 * self.w, HISTOGRAM_HEIGHT)
+        glVertex2f(0.5 * self.w, self.h)
+        glEnd()
+
+
     ## Draw the histogram of our data. 
     def drawHistogram(self):
         # White box over all
