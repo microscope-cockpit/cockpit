c597833f293de77c1551ba636ba50571f2bec52f
Mick Phillips <mick.phillips@gmail.com>
2015-10-28 17:21:15 +0000

    Drawer.getHandlers just returns empty handler.

--- devices/drawer.py
+++ devices/drawer.py
@@ -13,24 +13,10 @@ class DrawerDevice(device.Device):
 
 
     def getHandlers(self):
-        handler = handlers.drawer.DrawerHandler("drawer", "miscellaneous",
-                None, 0, None)
-        for camera, config in CAMERAS.iteritems():
-            filters = map(lambda d, w: {'dye': d, 'wavelength': w},
-                         config['dyes'],
-                         config['wavelengths'])
-            handler.addCamera(camera, filters)
-        
-        # Add the dummy cameras, too.
-        cameraNames = ('Dummy camera 1', 'Dummy camera 2', 'Dummy camera 3', 'Dummy camera 4')
-        drawerNames = ('Dummy drawer 1', 'Dummy drawer 2')
-        dyes = [('GFP', 'TRITC', 'mCherry', 'DAPI'), 
-                ('Cy5', 'FITC', 'Rhod', 'DAPI')]
-        wavelengths = [(525, 600, 585, 447), (695, 518, 590, 450)]
-        for i, camera in enumerate(cameraNames):
-            filters = map(lambda d, w: {'dye': d, 'wavelength': w}, 
-                [dye[i] for dye in dyes],
-                [wavelength[i] for wavelength in wavelengths])
-            handler.addCamera(camera, filters)
+        # Just return an empty handler for now. It will be configured
+        # after the cameras have been initialized.
+        self.handler = handlers.drawer.DrawerHandler("drawer", "miscellaneous",
+                                                       None, 0, None)
+        return [self.handler]
 
-        return [handler]
+
