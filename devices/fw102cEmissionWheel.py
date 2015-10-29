9d53c855978118064a08ed725290a8797643baba
Mick Phillips <mick.phillips@gmail.com>
2015-10-29 14:06:17 +0000

    Basic interaction with drawer handler implemented.

--- devices/fw102cEmissionWheel.py
+++ devices/fw102cEmissionWheel.py
@@ -19,34 +19,90 @@ limitations under the License.
 """
 import depot
 import device
-from config import config, CAMERAS
+import events
+from collections import namedtuple
+from config import config
+import re
 import wx
 
-CLASS_NAME = 'ThorlabsFilterWheelDevice'
-CONFIG_NAME = 'fw102c'
+CLASS_NAME = 'ThorlabsFilterWheelManager'
+CONFIG_NAME = 'emission wheel'
+CONFIG_DELIMETERS = '[,;:\-]+ ?'
+
+__DRAWERHANDLER__ = None
+
+Filter = namedtuple('Filter', 'dye, wavelength')
+
+
+class ThorlabsFilterWheelManager(device.Device):
+    def __init__(self):
+        device.Device.__init__(self)
+        # Must be initialized after the drawer.
+        self.priority = 500
+        names = [s for s in config.sections() if s.startswith(CONFIG_NAME)]
+        if not names:
+            self.isActive = False
+            return
+
+        self.isActive = True
+        self.wheels = []
+        for name in names:
+            self.wheels.append(ThorlabsFilterWheelDevice(name))
+
+    def initialize(self):
+        for w in self.wheels:
+            w.initialize()
+
+
+    def finalizeInitialization(self):
+        for w in self.wheels:
+            w.finalizeInitialization()
+
 
 
 class ThorlabsFilterWheelDevice(device.Device):
-    def __init__(self):
-        device.Device.__init__(self)
-        self.connection = None
-        self.cameraName = ''
-        self.drawerHandler = {}
-        self.filters = {}
+    def __init__(self, name):
+        self.name = name
+        self.cameraNames = []
+        self.filters = []
         self.timer = wx.Timer()
         self.timer.Bind(wx.EVT_TIMER, self.pollFunction)
-        self.timer.Start(1000)
-        wx.GetTopLevelWindows()[0].Bind(wx.EVT_IDLE, self.idle)
+        self.curPosition = None
 
 
-    def idle(self, evt):
-        print 'idle'
-
+    def finalizeInitialization(self):
+        # The drawer will now be initialized.
+        global __DRAWERHANDLER__
+        __DRAWERHANDLER__ = depot.getHandlersOfType(depot.DRAWER)[0]
+        print '__DRAWERHANDLER__: %s' % __DRAWERHANDLER__
+        # Cameras also initialized.
+        if config.has_option(self.name, 'camera'):
+            cameras = [config.get(self.name, 'camera')]
+        elif config.has_option(self.name, 'cameras'):
+            cameras = re.split(CONFIG_DELIMETERS, config.get(self.name, 'cameras'))
+        else:
+            raise Exception('%s: no camera(s) defined for %s' % (CLASS_NAME, self.name))
+        self.cameraNames = [c.name for c in depot.getHandlersOfType(depot.CAMERA)
+                                if c.name.lower() in cameras]
+        # Update the drawer.
+        self.updateDrawer()
 
 
     def initialize(self):
         # Read config to populate filters.
-        pass
+        numSlots = config.getint(self.name, 'slots')
+        self.filters = []
+        for i in range(1, numSlots+1):
+            if config.has_option(self.name, str(i)):
+                filterStr = config.get(self.name, str(i))
+                f = Filter(*re.split(CONFIG_DELIMETERS, filterStr))
+            else:
+                f = Filter(None, None)
+            self.filters.append(f)
+        # Get the current position.
+        self.curPosition = self.getPosition()
+        # Start the timer that will detect manual position changes.
+        #self.timer.Start(1000)
 
 
     def setFilter(self, filter):
@@ -56,6 +112,7 @@ class ThorlabsFilterWheelDevice(device.Device):
 
     def getPosition(self):
         # Fetch the current position.
+        return 0
         pass
 
 
@@ -64,13 +121,16 @@ class ThorlabsFilterWheelDevice(device.Device):
         pass
 
 
+    def updateDrawer(self):
+        h = __DRAWERHANDLER__
+        f = self.filters[self.curPosition]
+        for camera in self.cameraNames:
+            h.changeFilter(camera, f.dye, f.wavelength)
+        events.publish("drawer change", h)
+
+
     def pollFunction(self, evt):
-        print evt
-
-
-
-
-
-
-
-
+        lastPosition = self.curPosition
+        self.curPosition = self.getPosition()
+        if lastPosition != self.curPosition:
+            self.updateDrawer()
