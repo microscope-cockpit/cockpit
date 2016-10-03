c4451f440b9240aa7a69bbc038cf0f92f15985fd
Mick Phillips <mick.phillips@gmail.com>
2016-10-03 17:26:35 +0100

    Code cleanup in universalCamera.

--- devices/universalCamera.py
+++ devices/universalCamera.py
@@ -23,8 +23,6 @@ Supports cameras which implement the interface defined in
 
 import collections
 import decimal
-import numpy
-import threading
 import time
 import wx
 
@@ -53,20 +51,21 @@ class UniversalCameraDevice(camera.CameraDevice):
         super(UniversalCameraDevice, self).__init__(cam_config)
         self.handler = None        
         self.enabled = False
+        self.panel = None
         self.config = cam_config
         # Pyro proxy
-        self.proxy = Pyro4.Proxy('PYRO:%s@%s:%d' % 
-                                  ('Device',
+        self.proxy = Pyro4.Proxy('PYRO:%s@%s:%d' %
+                                 ('Device',
                                    cam_config.get('ipAddress') or cam_config.get('host'),
                                    cam_config.get('port')))
-        self.listener = util.listener.Listener(self.object, 
+        self.listener = util.listener.Listener(self.proxy,
                                                lambda *args: self.receiveData(*args))
         self.base_transform = cam_config.get('baseTransform') or (0, 0, 0)
         self.path_transform = (0, 0, 0)
         self.settings = {}
         self.cached_settings={}
         self.settings['transform'] = tuple(
-                         path_transform[i] ^ baseTransform[i] for in range(3))
+                         path_transform[i] ^ baseTransform[i] for i in range(3))
         self.settings['exposure_time'] = 0.001
 
 
@@ -75,8 +74,8 @@ class UniversalCameraDevice(camera.CameraDevice):
         if self.enabled:
             self.settings.update(self.cached_settings)
             self.proxy.update_settings(self.settings)
-            self.object.enable()
-        self.handler.exposureMode = self.interactiveTrigger
+            self.proxy.enable()
+        self.handler.exposureMode = self.proxy.get_trigger_type()
 
 
     def performSubscriptions(self):
@@ -91,7 +90,7 @@ class UniversalCameraDevice(camera.CameraDevice):
         self.settings.update({'pathTransform': transform})
         # Apply the change now if the camera is enabled.
         if self.enabled:
-            self.object.update_settings(self.settings)
+            self.proxy.update_settings(self.settings)
     
 
     def getHandlers(self):
@@ -108,7 +107,7 @@ class UniversalCameraDevice(camera.CameraDevice):
                     'setImageSize': self.setImageSize,
                     'getSavefileInfo': self.getSavefileInfo,
                     'makeUI': self.makeUI},
-                self.interactiveTrigger)
+                TRIGGER_SOFT) # will be set with value from hardware later
         self.handler = result
         return result
 
@@ -119,8 +118,8 @@ class UniversalCameraDevice(camera.CameraDevice):
             # Disable the camera, if it is enabled.
             if self.enabled:
                 self.enabled = False
-                self.object.disable()
-                self.object.make_safe()
+                self.proxy.disable()
+                self.proxy.make_safe()
                 self.listener.disconnect()
                 self.updateUI()
                 return
@@ -129,32 +128,28 @@ class UniversalCameraDevice(camera.CameraDevice):
         if self.enabled:
             # Nothing to do.
             return
+        # Use async call to allow hardware time to respond.
+        result = Pyro4.async(proxy).enable()
+        result.iferror(self.onPyroError)
+        result.then(self.onEnableComplete)
 
 
-        originalTimeout = self.object._pyroTimeout
-        try:
-            self.object._pyroTimeout = 60
-            self.object.enable(self.settings)
-            self.object._pyroTimeout = originalTimeout
-        except Exception as e:
-            print e
-        else:
-            # Wait for camera to show it is enabled.
-            while not self.object.is_enabled():
-                time.sleep(1)
-            self.enabled = True
-            # Connect the listener to receive data.
-            self.listener.connect()
-            # Update our settings with the real settings.
-            self.settings.update(self.object.get_settings())
-        # Update the UI.
+    def onEnableComplete(self):
+        self.settings.update(self.proxy.get_all_settings())
+        self.listener.connect()
+        self.enabled = True
         self.updateUI()
+
+
+    def onPyroError(err):
+        """Handle exceptions raised by aync. proxy."""
+        raise err
 
 
     def getExposureTime(self, name, isExact):
         """Read the real exposure time from the camera."""
         # Camera uses times in s; cockpit uses ms.
-        t = self.object.get_exposure_time()
+        t = self.proxy.get_exposure_time()
         if isExact:
             return decimal.Decimal(t) * (decimal.Decimal(1000.0))
         else:
@@ -163,21 +158,18 @@ class UniversalCameraDevice(camera.CameraDevice):
 
     def getImageSize(self, name):
         """Read the image size from the camera."""
-        return self.object.get_image_size()
+        return self.proxy.get_roi_size()
 
 
     def getImageSizes(self, name):
         """Return a list of available image sizes."""
-        return self.imageSizes
+        return None
 
 
     def getSavefileInfo(self, name):
         """Return an info string describing the measurement."""
-        if self.settings.get('amplifierMode').get('amplifier') == 0:
-            gain = 'EM %d' % self.settings.get('EMGain')
-        else:
-            gain = 'Conv'
-        return "%s: %s gain, %s image" % (name, gain, '512x512')
+        #return "%s: %s image" % (name, self.imageSize)
+        return ""
 
 
     def getTimeBetweenExposures(self, name, isExact=False):
@@ -186,7 +178,7 @@ class UniversalCameraDevice(camera.CameraDevice):
         This is the time that must pass after stopping one exposure
         before another can be started, in milliseconds."""
         # Camera uses time in s; cockpit uses ms.
-        t = self.object.get_min_time_between_exposures() * 1000.0
+        t = self.proxy.get_cycle_time() * 1000.0
         if isExact:
             result = decimal.Decimal(t)
         else:
@@ -197,33 +189,18 @@ class UniversalCameraDevice(camera.CameraDevice):
     def prepareForExperiment(self, name, experiment):
         """Make the hardware ready for an experiment."""
         self.cached_settings.update(self.settings)
-        self.object.abort()
-        self.settings['frameTransfer'] = self.experimentTriggerMode.frameTransfer
-        self.settings['fastTrigger'] = self.experimentTriggerMode.fastTrigger
-        self.settings['triggerMode'] = self.experimentTriggerMode.cameraTrigger
-        try:
-            self.object.enable(self.settings)
-        except:
-            raise
-        else:
-            self.handler.exposureMode = self.experimentTriggerMode.cockpitTrigger
 
 
-    def receiveData(self, action, *args):
+    def receiveData(self, *args):
         """This function is called when data is received from the hardware."""
-        # print 'receiveData received %s' % action
-        if action == 'new image':
-            (image, timestamp) = args
-            events.publish('new image %s' % self.name, image, timestamp)
+        (image, timestamp) = args
+        events.publish('new image %s' % self.name, image, timestamp)
 
 
     def setExposureTime(self, name, exposureTime):
         """Set the exposure time."""
         # Camera uses times in s; cockpit uses ms.
-        self.settings.update({'exposureTime': exposureTime / 1000.0})
-        # Apply the change right now if the camera is enabled.
-        if self.enabled:
-            self.object.update_settings(self.settings)
+        self.proxy.set_exposure_time(exposureTime / 1000.0)
 
 
     def setImageSize(self, name, imageSize):
@@ -231,44 +208,8 @@ class UniversalCameraDevice(camera.CameraDevice):
 
 
     ### UI stuff ###
-    def onGainButton(self, event=None):
-        menu = wx.Menu()
-        menuID = 1
-        for value in range (0, 255, 10):
-            menu.Append(menuID, str(value))
-            wx.EVT_MENU(self.panel, menuID, lambda event, value=value: self.setGain(value))
-            menuID += 1
-        gui.guiUtils.placeMenuAtMouse(self.panel, menu)
-
-
-    def setGain(self, value):
-        self.settings.update({'EMGain': value})
-        # Apply the change now if the camera is enabled.
-        if self.enabled:
-            self.object.update_settings(self.settings)
-        self.updateUI()
-
-
-    def onModeButton(self, event=None):
-        menu = wx.Menu()
-        if not self.amplifierModes:
-            # Camera not enabled yet.
-            menu.Append(0, str('No modes known - camera never enabled.'))
-            wx.EVT_MENU(self.panel, 0, None)
-        else:
-            menuID = 0
-            for mode in self.amplifierModes:
-                menu.Append(menuID, mode['label'])
-                #wx.EVT_MENU(self.panel, menuID, lambda event, n=menuID:
-                #            self.setAmplifierMode(n))
-                wx.EVT_MENU(self.panel, menuID, lambda event, m=mode:
-                            self.setAmplifierMode(m))
-                menuID += 1
-        gui.guiUtils.placeMenuAtMouse(self.panel, menu)
-
-
     def onRightMouse(self, event=None):
-        """Present a thermal management menu on right click."""
+        """Present a panel of settings on right click."""
         menu = wx.Menu()
         menu.SetTitle('Thermal management')
 
@@ -277,138 +218,21 @@ class UniversalCameraDevice(camera.CameraDevice):
         menu.Check(0, self.settings.get('isWaterCooled', False))
         wx.EVT_MENU(self.panel, 0, lambda event: self.toggleWaterCooling())
 
-        # Submenu of temperature set points.
-        tMenu = wx.Menu()
-        temperatures = [-40, -50, -60, -70, -80, -90, -100]
-        airCooledLimit = -50
-        for itemID, t in enumerate(temperatures, 100):
-            tMenu.AppendRadioItem(itemID, u'%d°C' % t)
-            if t == self.settings['targetTemperature']:
-                tMenu.Check(itemID, True)
-            if t < airCooledLimit and not self.settings.get('isWaterCooled'):
-                tMenu.Enable(itemID, False)
-            wx.EVT_MENU(self.panel, itemID, lambda event, target=t: self.setTargetTemperature(target))
-            itemID += 1
-
         menu.AppendSubMenu(tMenu, 'sensor set point')
 
         gui.guiUtils.placeMenuAtMouse(self.panel, menu)
 
 
-    def onTrigButton(self, event=None):
-        menu = wx.Menu()
-        menuID = 0
-        for mode in TRIGGER_MODES:
-            menu.Append(menuID, mode.label)
-            wx.EVT_MENU(self.panel, menuID, lambda event, m=mode:
-                        self.setExperimentTriggerMode(m))
-            menuID += 1
-        gui.guiUtils.placeMenuAtMouse(self.panel, menu)
-
-
-    def setExperimentTriggerMode(self, mode):
-        self.experimentTriggerMode = mode
-        self.trigButton.SetLabel('exp. trigger:\n%s' % mode.label)
-
-
-    def setAmplifierMode(self, mode):
-        self.settings.update({'amplifierMode': mode})
-        # Apply the change right now if camera is enabled.
-        if self.enabled:
-            self.object.update_settings(self.settings)
-        self.updateUI()
-
-
-    def setTargetTemperature(self, temperature):
-        self.settings.update({'targetTemperature': temperature})
-        self.object.update_settings({'targetTemperature': temperature})
-
-
-    def toggleWaterCooling(self):
-        newSetting = not self.settings.get('isWaterCooled')
-        self.settings.update({'isWaterCooled': newSetting})
-        self.object.update_settings({'isWaterCooled': newSetting})
-
-
-    ### UI functions ###
+   ### UI functions ###
     def makeUI(self, parent):
         self.panel = wx.Panel(parent)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        label = gui.device.Label(
-                parent=self.panel, label=self.config['label'])
-        sizer.Add(label)
-        rowSizer = wx.BoxSizer(wx.VERTICAL)
-
-        self.modeButton = gui.toggleButton.ToggleButton(
-                label="Mode:\n%s" % 'not set',
-                parent=self.panel)
-        self.modeButton.Bind(wx.EVT_LEFT_DOWN, self.onModeButton)
-        self.modeButton.Unbind(wx.EVT_RIGHT_DOWN)
-        rowSizer.Add(self.modeButton)
-
-        self.gainButton = gui.toggleButton.ToggleButton(
-                label="EM Gain\n%d" % self.settings['EMGain'],
-                parent=self.panel)
-        self.gainButton.Bind(wx.EVT_LEFT_DOWN, self.onGainButton)
-        self.gainButton.Unbind(wx.EVT_RIGHT_DOWN)
-        rowSizer.Add(self.gainButton)
-
-        self.trigButton = gui.toggleButton.ToggleButton(
-                label='exp. trigger:\n%s' % self.experimentTriggerMode.label,
-                parent=self.panel)
-        self.trigButton.Bind(wx.EVT_LEFT_DOWN, self.onTrigButton)
-        self.trigButton.Unbind(wx.EVT_RIGHT_DOWN)
-        rowSizer.Add(self.trigButton)
-
-        sizer.Add(rowSizer)
-        self.panel.SetSizerAndFit(sizer)
-        for child in self.panel.Children:
-            child.Bind(wx.EVT_RIGHT_DOWN, self.onRightMouse)
-        self.hasUI = True
         return self.panel
 
 
     def updateUI(self):
-        if not self.hasUI:
+        if not self.panel:
             # No UI to update.
             return
-        # If there is no local amplifierMode
-        mode = self.settings.get('amplifierMode', None)
-        if not mode:
-            try:
-                # try to read it from the hardware
-                mode = self.object.get_settings()['amplifierMode']
-            except:
-                mode = None
-
-        # If we succeeded in retrieving a mode ...
-        if mode:
-            # fetch the mode description ...
-            modeString = mode['label']
-            # and figure out of it uses EM.
-            modeIsEM = mode['amplifier'] == 0
-        else:
-            # Otherwise, show that we have no mode description ...
-            modeString = 'not set'
-            # and assume no EM.
-            modeIsEM = False
-
-        # Light up the mode button if the camera is active.
-        if self.enabled:
-            self.modeButton.setActive(True)
-        else:
-            self.modeButton.setActive(False)
-
-        # Light up the gain button if camera is active and mode uses EM.
-        if modeIsEM and self.enabled:
-            self.gainButton.setActive(True)
-        else:
-            self.gainButton.setActive(False)
-
-        # Labels must be set after setActive call, or the original
-        # label persists.
-        self.modeButton.SetLabel('Mode:\n%s' % modeString)
-        self.gainButton.SetLabel('EM Gain:\n%d' % self.settings['EMGain'])
 
 
 class UniversalCameraManager(camera.CameraManager):
