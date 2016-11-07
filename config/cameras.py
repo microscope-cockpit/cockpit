55255e129e53e98e3cab1ff5475f09818d6d1774
Mick Phillips <mick.phillips@gmail.com>
2016-11-07 17:30:20 +0000

    Universal test camera in config.

--- config/cameras.py
+++ config/cameras.py
@@ -1,5 +1,6 @@
 cameras = [
     #('West', 9146, 1<<0, '127.0.0.1', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585]),
     #('East', 9145, 1<<1, '127.0.0.1', 7776, 'ixon', ['Cy5', 'FITC'], [670, 518]),
+    ('Test', 0000, 0, '127.0.0.1', 8001, 'universalCamera', ['GFP'], [252,]),
     ]
 camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
