microscope-cockpit/cockpit

commit 0043c49a4fa95bf64b3b3e1df9ab7534f7d3c9d1

add an event for exposure time changes

handlers/lightSource.py
@@ -197,7 +197,8 @@ class LightHandler(deviceHandler.DeviceHandler):
     @reset_cache
     def setExposureTime(self, value):
         self.callbacks['setExposureTime'](self.name, value)
-        events.publish('laser exposure update')
+        events.publish('laser exposure update',self)
+        self.setLabel()
 
 
     ## Get the current exposure time, in milliseconds.

commit 38e6bad814a5850373fa116b57b4bb5bc8456740

Fixed bug introduced in 0043c49a4fa.
Functions subscribed to laser exposure update events do
not expect further arguments.

handlers/lightSource.py
@@ -197,7 +197,7 @@ class LightHandler(deviceHandler.DeviceHandler):
     @reset_cache
     def setExposureTime(self, value):
         self.callbacks['setExposureTime'](self.name, value)
-        events.publish('laser exposure update',self)
+        events.publish('laser exposure update')
         self.setLabel()
 
 