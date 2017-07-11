8456fc6dc5351483a348868280476e1501b2b69b
unknown <whgu0532@bioch3152.bioch.ox.ac.uk>
2017-07-11 11:09:27 +0100

    Using Ian's config.

--- config/analog.py
+++ config/analog.py
@@ -11,23 +11,23 @@ aout_keys = ['name',
 aouts = [(
             'polrot',   # polarisation rotatoar
             'SI angle',    # moves angle 
-            2,          # on analogue out 2
+            1,          # on analogue out 2
             1,          # 1V per V
-            (0, 10),
-            (None, None),
-            [0.01, 0.05, 0.1, 0.5, 1],
+            (.5, 10),
+            (0.5, 10),
+            [1.91,2.13,1.75], # overloaded as pol-rotator voltages
             2,
-            None
+            2.0
          ),
          (
             'z_insert',
             'z',
             0,          # on analogue out 0
-            20,         # microns / V
-            (0, 100),
-            (0, 91.13),
+            25,         # microns / V
+            (0, 250),
+            (0, 250),
             [.05, .1, .5, 1, 2, 5, 10],
             2,
-            45,
+            100,
          )  
         ]
--- config/cameras.py
+++ config/cameras.py
@@ -1,5 +1,9 @@
+
 cameras = [
-    #('West', 9146, 1<<0, '127.0.0.1', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585]),
-    #('East', 9145, 1<<1, '127.0.0.1', 7776, 'ixon', ['Cy5', 'FITC'], [670, 518]),
-]
-camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
+    ('West', 9146, 1<<8, '192.168.1.2', 7777, 'ixon', ['TRITC'], [600], (0,0,1)),
+    ('East', 9145, 1<<9, '192.168.1.2', 7778, 'ixon', ['GFP'], [525], (0,0,1)),
+    ("North", 'VSC-01344', 1<<10, '192.168.1.2',8192, 'universal', ['ZYLA'], [525],
+    (0, 0, 1)),
+    ]
+camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths','baseTransform']
+#transform order [0]=rot90,[1]=flip_h,[2]=flip_v
