70c674ec0fb80e922bf274006d829c95121f894e
Carnë Draug <carandraug+dev@gmail.com>
2017-12-11 19:14:50 +0000

    util/Mrc.py: specify the BSD license
    This module was copied from Priithon and refers to that package
    LICENSE file.  Copy that license into the file header so we know
    its license.

--- util/Mrc.py
+++ util/Mrc.py
@@ -1,3 +1,31 @@
+## Copyright (c) 2005 The Regents of the University of California
+##
+## All rights reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+##   a. Redistributions of source code must retain the above copyright notice,
+##      this list of conditions and the following disclaimer.
+##   b. Redistributions in binary form must reproduce the above copyright
+##      notice, this list of conditions and the following disclaimer in the
+##      documentation and/or other materials provided with the distribution.
+##   c. Neither the name of the University of California, San Francisco nor
+##      the names of its contributors may be used to endorse or promote products
+##      derived from this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+## ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+## BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+## OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+## SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+## INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+## THE POSSIBILITY OF SUCH DAMAGE.
+
 """MRC file format: refer to
 http://www.msg.ucsf.edu/IVE/IVE4_HTML/IM_ref2.html
 
@@ -5,7 +33,7 @@ Mrc class uses memory mapping (file size limit about 1GB (more or less)
 Mrc2 class section wise file/array I/O
 """
 __author__  = "Sebastian Haase <haase@msg.ucsf.edu>"
-__license__ = "BSD license - see LICENSE file"
+__license__ = "3-clause BSD license"
 
 import numpy as N
 
