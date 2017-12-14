d9c2a2716d0ced356a642e6eba15f02392cdd715
Mick Phillips <mick.phillips@gmail.com>
2017-12-14 15:49:08 +0000

    Added missing delay when falling back to software.

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -260,6 +260,11 @@ class Experiment:
 
                     util.logger.log.debug(
                         "Running 1 line in software for %s with %d reps at %.2f" % (h, numReps, time.time()))
+
+                    lastTime = self.table[curIndex-1][0]
+                    while(time.time() < (startTime + float(lastTime)/1000)):
+                        time.sleep(0.001)
+
                     fn()
                     curIndex += 1
                 else:
