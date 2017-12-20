fa4973fc2c928078e0d8050838f94cae77cc4fa2
Mick Phillips <mick.phillips@gmail.com>
2017-12-20 14:32:01 +0000

    Cleared out obsolete config stuff.

--- config/cameras.py
+++
@@ -1,5 +0,0 @@
-cameras = [
-    #('West', 9146, 1<<0, '127.0.0.1', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585]),
-    #('East', 9145, 1<<1, '127.0.0.1', 7776, 'ixon', ['Cy5', 'FITC'], [670, 518]),
-]
-camera_keys = ['label', 'serial', 'triggerLine', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
