972689fa7474d0883480116e8e0819cb794a1eba
Mick Phillips <mick.phillips@gmail.com>
2018-01-24 10:48:45 +0000

    Fixed indentation and added comment.

--- gui/drawShapes.py
+++ gui/drawShapes.py
@@ -59,66 +59,66 @@ def drawTextAt(self, loc, text, size, color = (0, 0, 0)):
     glPopMatrix()
 
 
-
+## Draw a circle of radius r centred on x0, y0 with n vertices.
 def drawScaledCircle(self, x0, y0, r, n, offset=True):
-        dTheta = 2. * PI / n
-        cosTheta = numpy.cos(dTheta)
-        sinTheta = numpy.sin(dTheta)
-        if offset:
-            x0=x0-self.offset[0]
-            y0 =y0+self.offset[1]
-        x = r
-        y = 0.
+    dTheta = 2. * PI / n
+    cosTheta = numpy.cos(dTheta)
+    sinTheta = numpy.sin(dTheta)
+    if offset:
+        x0=x0-self.offset[0]
+        y0 =y0+self.offset[1]
+    x = r
+    y = 0.
 
-        glBegin(GL_LINE_LOOP)
-        for i in xrange(n):
-            glVertex2f(-(x0 + x), y0 + y)
-            xOld = x
-            x = cosTheta * x - sinTheta * y
-            y = sinTheta * xOld + cosTheta * y
-        glEnd()
+    glBegin(GL_LINE_LOOP)
+    for i in xrange(n):
+        glVertex2f(-(x0 + x), y0 + y)
+        xOld = x
+        x = cosTheta * x - sinTheta * y
+        y = sinTheta * xOld + cosTheta * y
+    glEnd()
 
-    ## Draw a rectangle centred on x0, y0 of width w and height h.
-    def drawScaledRectangle(self, x0, y0, w, h, offset=True):
-        dw = w / 2.
-        dh = h / 2.
-        if offset:
-            x0 = x0-self.offset[0]
-            y0 = y0+self.offset[1]
-        ps = [(x0-dw, y0-dh),
-              (x0+dw, y0-dh),
-              (x0+dw, y0+dh),
-              (x0-dw, y0+dh)]
 
-        glBegin(GL_LINE_LOOP)
-        for i in xrange(-1, 4):
-            glVertex2f(-ps[i][0], ps[i][1])
-        glEnd()
-    # Draw a crosshairs at the specified position with the specified color.
-    # By default make the size of the crosshairs be really big.
-    def drawCrosshairs(self, position, color, size = None):
-        xSize = ySize = size
-        if size is None:
-            xSize = ySize = 100000
-        x, y = position
-        #if no offset defined we can't apply it!
-        if hasattr(self, 'offset'):
-            x = x-self.offset[0]
-            y = y-self.offset[1]
+## Draw a rectangle centred on x0, y0 of width w and height h.
+def drawScaledRectangle(self, x0, y0, w, h, offset=True):
+    dw = w / 2.
+    dh = h / 2.
+    if offset:
+        x0 = x0-self.offset[0]
+        y0 = y0+self.offset[1]
+    ps = [(x0-dw, y0-dh),
+          (x0+dw, y0-dh),
+          (x0+dw, y0+dh),
+          (x0-dw, y0+dh)]
 
-        # Draw the crosshairs
-        glColor3f(*color)
-        glBegin(GL_LINES)
-        glVertex2d(-x - xSize, y)
-        glVertex2d(-x + xSize, y)
-        glVertex2d(-x, y - ySize)
-        glVertex2d(-x, y + ySize)
-        glEnd()
+    glBegin(GL_LINE_LOOP)
+    for i in xrange(-1, 4):
+        glVertex2f(-ps[i][0], ps[i][1])
+    glEnd()
+# Draw a crosshairs at the specified position with the specified color.
+# By default make the size of the crosshairs be really big.
+def drawCrosshairs(self, position, color, size = None):
+    xSize = ySize = size
+    if size is None:
+        xSize = ySize = 100000
+    x, y = position
+    #if no offset defined we can't apply it!
+    if hasattr(self, 'offset'):
+        x = x-self.offset[0]
+        y = y-self.offset[1]
 
-        glBegin(GL_LINE_LOOP)
-        # Draw the box.
-        for i, j in [(-1, -1), (-1, 1), (1, 1), (1, -1)]:
-            glVertex2d(-x + i * self.crosshairBoxSize / 2,
-                    y + j * self.crosshairBoxSize / 2)
-        glEnd()
+    # Draw the crosshairs
+    glColor3f(*color)
+    glBegin(GL_LINES)
+    glVertex2d(-x - xSize, y)
+    glVertex2d(-x + xSize, y)
+    glVertex2d(-x, y - ySize)
+    glVertex2d(-x, y + ySize)
+    glEnd()
 
+    glBegin(GL_LINE_LOOP)
+    # Draw the box.
+    for i, j in [(-1, -1), (-1, 1), (1, 1), (1, -1)]:
+        glVertex2d(-x + i * self.crosshairBoxSize / 2,
+                y + j * self.crosshairBoxSize / 2)
+    glEnd()
