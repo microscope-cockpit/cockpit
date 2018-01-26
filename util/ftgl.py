cc02a598d8dd765937a1be249fb57c7350f649f8
Carnë Draug <carandraug+dev@gmail.com>
2018-01-26 15:34:44 +0000

    util/ftgl.py: cross-platform and test cases

--- util/ftgl.py
+++ util/ftgl.py
@@ -1,48 +1,72 @@
 import ctypes
-from ctypes import WINFUNCTYPE, WinDLL, c_void_p, c_uint, c_int, c_char_p
+import os
 
-_lib = WinDLL("ftgl")
-#prototype = ctypes.WINFUNCTYPE(ctypes.c_void_p, ctypes.c_char_p)
-#f = prototype(getattr(lib, "??0FTTextureFont@@QEAA@PEBD@Z"))
-#TextureFont = ctypes.WINFUNCTYPE(ctypes.c_void_p, ctypes.c_char_p)(getattr(lib, "??0FTTextureFont@@QEAA@PEBD@Z"))
-#setFontFaceSize = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.c_long_p, ctypes.c_int)(getattr(lib, "??0FTTextureFont@@QEAA@PEBD@Z"))
+from ctypes import POINTER, c_char_p, c_int, c_uint
 
-# FTGLfont *ftglCreateTextureFont(const char *file);
-pr = WINFUNCTYPE(c_void_p, c_char_p)
-pf = ((1, "filename"),)
-_createTextureFont = pr(('ftglCreateTextureFont', _lib), pf)
+if os.name in ('nt', 'ce'):
+    _ftgl = ctypes.WinDLL("ftgl")
+else:
+    _ftgl = ctypes.CDLL("libftgl.so")
 
-#setFontFaceSize = ctypes.WINFUNCTYPE(ctypes.c_int, ctypes.c_void_p, ctypes.c_int, ctypes.c_int)(lib.ftglSetFontFaceSize)
+class _FTGLfont(ctypes.Structure):
+    pass
 
-# uint ftglGetFontFaceSize (FTGLfont *font) 
-# Get the current face size in points (1/72 inch). 
-pr = WINFUNCTYPE(c_uint, c_void_p)
-pf = ((1, "FTGLfont"),)
-_getFontFaceSize = pr(('ftglGetFontFaceSize', _lib), pf)
+## macros for rendering modes in FTGL/ftgl.h
+_FTGL_RENDER_FRONT = c_int(0x0001)
+_FTGL_RENDER_BACK = c_int(0x0002)
+_FTGL_RENDER_SIDE = c_int(0x0004)
+_FTGL_RENDER_ALL = c_int(0xffff)
 
-# int ftglSetFontFaceSize (FTGLfont *font, unsigned int size, unsigned int res)
-# Set the char size for the current face. 
-pr = WINFUNCTYPE(c_int, c_void_p, c_uint, c_uint)
-pf = ((1, "FTGLfont"), (1, "size"), (5, "res"))
-_setFontFaceSize = pr(('ftglSetFontFaceSize', _lib), pf)
+## FT_Error is a typedef for a freetype enum.  Typical Freetype builds
+## will not include a map for error number to error message.  Instead,
+## they provide macros for clients to build the map themselves.  FTGL
+## does not use it.  So we will only check if it's different from 0.
+## See https://www.freetype.org/freetype2/docs/reference/ft2-error_enumerations.html
+_FT_Error = c_int
 
-# void ftglRenderFont (FTGLfont *font, const char *string, int mode)
-# Render a string of characters. 
-# enum    RenderMode { RENDER_FRONT = 0x0001, RENDER_BACK = 0x0002, RENDER_SIDE = 0x0004, RENDER_ALL = 0xffff }
-pr = WINFUNCTYPE(c_void_p, c_void_p, c_char_p, c_int)
-pf = ((1, "FTGLfont"), (1, "text"), (1, "mode"))
-_renderFont = pr(('ftglRenderFont', _lib), pf)
+
+## FTGLfont* ftglCreateTextureFont (const char* file)
+_createTextureFont = _ftgl.ftglCreateTextureFont
+_createTextureFont.argtypes = [c_char_p]
+_createTextureFont.restype = POINTER(_FTGLfont)
+
+## unsigned int ftglGetFontFaceSize (FTGLfont* font)
+_getFontFaceSize = _ftgl.ftglGetFontFaceSize
+_getFontFaceSize.argtypes = [POINTER(_FTGLfont)]
+_getFontFaceSize.restype = c_uint
+
+## void ftglRenderFont (FTGLfont *font, const char *string, int mode)
+_renderFont = _ftgl.ftglRenderFont
+_renderFont.argtypes = [POINTER(_FTGLfont), c_char_p, c_int]
+_renderFont.restype = None
+
+## int ftglSetFontFaceSize (FTGLfont* font, unsigned int size, unsigned int res)
+_setFontFaceSize = _ftgl.ftglSetFontFaceSize
+_setFontFaceSize.argtypes = [POINTER(_FTGLfont), c_uint, c_uint]
+_setFontFaceSize.restype = c_int
+
+## FT_Error ftglGetFontError (FTGLfont* font)
+_getFontError = _ftgl.ftglGetFontError
+_getFontError.argtypes = [POINTER(_FTGLfont)]
+_getFontError.restype = _FT_Error
 
 
 class TextureFont(object):
     def __init__(self, path):
         self._font = _createTextureFont(path.encode('ascii'))
+        if not self._font:
+            raise RuntimeError("failed to create texture font from '%s'" % path)
 
     def getFaceSize(self):
         return _getFontFaceSize(self._font)
 
-    def setFaceSize(self, size):
-        return _setFontFaceSize(self._font, size)
+    def setFaceSize(self, size, res=5):
+        if _setFontFaceSize(self._font, size, res) != 1:
+            raise RuntimeError("failed to set font to size '%i' with res '%i'"
+                               % (size, res))
 
-    def render(self, text):
-        return _renderFont(self._font, text.encode('ascii'), 0xffff)
+    def render(self, text, mode=_FTGL_RENDER_ALL):
+        _renderFont(self._font, text.encode('ascii'), mode)
+        err = _getFontError(self._font)
+        if err != 0:
+            raise RuntimeError("failed to render '%s'", text)
