96f15c9e54dadd08e5316c066bda7ebd50c8b3f1
Mick Phillips <mick.phillips@gmail.com>
2018-02-02 15:13:10 +0000

    Removed obsolete sample configs.

--- sample-configs/cameras.py
+++
@@ -1,6 +0,0 @@
-## _cameras [(label, dsp line, ip address, model)]
-cameras = [
-    ('West', 1<<0, '127.0.0.1', 7777, 'ixon', ['GFP', 'mCherry'], [525, 585]),
-    ('East', 1<<1, '127.0.0.1', 7776, 'ixon', ['Cy5', 'FITC'], [670, 518]),
-    ]
-camera_keys = ['label', 'line', 'ipAddress', 'port', 'model', 'dyes', 'wavelengths']
