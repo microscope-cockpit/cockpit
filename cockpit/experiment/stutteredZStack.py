6a81f78781404b16c09402b738e80e7b6c442881
Mick Phillips <mick.phillips@gmail.com>
2018-08-29 14:45:22 +0100

    Interval and numReps may be blank.

--- cockpit/experiment/stutteredZStack.py
+++ cockpit/experiment/stutteredZStack.py
@@ -155,12 +155,14 @@ class ExperimentUI(wx.Panel):
         self.interval.SetToolTip(wx.ToolTip("Amount of time, in seconds, that " +
                 "passes between each rep for this portion of the experiment."))
         self.interval.SetValidator(FLOATVALIDATOR)
+        self.interval.allowEmpty = True
         sizer.Add(self.interval)
 
         self.numReps = wx.TextCtrl(self, -1, size = (60, -1))
         self.numReps.SetToolTip(wx.ToolTip("Number of reps to perform at this " +
                 "sampling interval."))
         self.numReps.SetValidator(INTVALIDATOR)
+        self.numReps.allowEmpty = True
         sizer.Add(self.numReps)
 
         button = wx.Button(self, -1, 'Add')
