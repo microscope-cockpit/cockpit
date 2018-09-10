9c2ff151adc2d30b1d06f5fedf3d17918ea08c90
NickHallONBI <nicholas.hall@dtc.ox.ac.uk>
2018-09-10 22:05:39 +0100

    Added start of RFcalibration script. Just Z stack currently

---
+++ cockpit/experiment/remote_focus_calibration.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+## Copyright (C) 2018 Mick Phillips <mick.phillips@gmail.com> &
+## Nicholas Hall <nicholas.hall@dtc.ox.ac.uk>
+##
+## This file is part of Cockpit.
+##
+## Cockpit is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Cockpit is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Cockpit.  If not, see <http://www.gnu.org/licenses/>.
+
+## Copyright 2013, The Regents of University of California
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions
+## are met:
+##
+## 1. Redistributions of source code must retain the above copyright
+##   notice, this list of conditions and the following disclaimer.
+##
+## 2. Redistributions in binary form must reproduce the above copyright
+##   notice, this list of conditions and the following disclaimer in
+##   the documentation and/or other materials provided with the
+##   distribution.
+##
+## 3. Neither the name of the copyright holder nor the names of its
+##   contributors may be used to endorse or promote products derived
+##   from this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+## FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+## COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+## INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+## BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+## LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+## CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+## LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+## ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+## POSSIBILITY OF SUCH DAMAGE.
+
+
+from . import actionTable
+from . import experiment
+
+import decimal
+import math
+
+## Provided so the UI knows what to call this experiment.
+EXPERIMENT_NAME = 'Remote Focus Z-stack Calibration'
+
+
+## This class handles classic Z-stack experiments.
+class RFZStackCalibration(experiment.Experiment):
+    ## Create the ActionTable needed to run the experiment. We simply move to
+    # each Z-slice in turn, take an image, then move to the next.
+    def generateActions(self):
+        table = actionTable.ActionTable()
+        curTime = 0
+        prevAltitude = None
+        numZSlices = int(math.ceil(self.zHeight / self.sliceHeight))
+        if self.zHeight > 1e-6:
+            # Non-2D experiment; tack on an extra image to hit the top of
+            # the volume.
+            numZSlices += 1
+        for zIndex in range(numZSlices):
+            # Move to the next position, then wait for the stage to
+            # stabilize.
+            zTarget = self.zStart + self.sliceHeight * zIndex
+            motionTime, stabilizationTime = 0, 0
+            if prevAltitude is not None:
+                motionTime, stabilizationTime = self.zPositioner.getMovementTime(prevAltitude, zTarget)
+            curTime += motionTime
+            table.addAction(curTime, self.zPositioner, zTarget)
+            curTime += stabilizationTime
+            prevAltitude = zTarget
+
+            # Image the sample.
+            for cameras, lightTimePairs in self.exposureSettings:
+                curTime = self.expose(curTime, cameras, lightTimePairs, table)
+                # Advance the time very slightly so that all exposures
+                # are strictly ordered.
+                curTime += decimal.Decimal('1e-10')
+            # Hold the Z motion flat during the exposure.
+            table.addAction(curTime, self.zPositioner, zTarget)
+
+        # Move back to the start so we're ready for the next rep.
+        motionTime, stabilizationTime = self.zPositioner.getMovementTime(
+                self.zHeight, 0)
+        curTime += motionTime
+        table.addAction(curTime, self.zPositioner, self.zStart)
+        # Hold flat for the stabilization time, and any time needed for
+        # the cameras to be ready. Only needed if we're doing multiple
+        # reps, so we can proceed immediately to the next one.
+        cameraReadyTime = 0
+        if self.numReps > 1:
+            for cameras, lightTimePairs in self.exposureSettings:
+                for camera in cameras:
+                    cameraReadyTime = max(cameraReadyTime,
+                            self.getTimeWhenCameraCanExpose(table, camera))
+        table.addAction(max(curTime + stabilizationTime, cameraReadyTime),
+                self.zPositioner, self.zStart)
+
+        return table
+
+
+
+## A consistent name to use to refer to the class itself.
+EXPERIMENT_CLASS = RFZStackCalibration
