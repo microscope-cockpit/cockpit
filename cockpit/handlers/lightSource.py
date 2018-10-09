b0042fa56280c92a1114591d8d89084af11478f8
Mick Phillips <mick.phillips@gmail.com>
2018-10-09 14:46:47 +0100

    DelegateTrigger can now do other actions.
    Triggers are delegated to some trigger source, which creates a trigger
    handler to distinguish between the DelegateTrigger handler and its trigger
    in the actionTable. This means that DelegateTrigger can also appear in the
    actionTable, and can perform actions with its own executeTable method: for
    example, the experiment can drop down from hardware triggered operation to
    run some calibration or configuration routine in software, then resume
    hardware triggering.

--- cockpit/handlers/lightSource.py
+++ cockpit/handlers/lightSource.py
@@ -118,8 +118,8 @@ class LightHandler(deviceHandler.DeviceHandler):
         self.exposureTime = None
         # Set up trigger handling.
         if trigHandler and trigLine:
-            trigHandler.registerDigital(self, trigLine)
-            self.triggerNow = lambda: trigHandler.triggerDigital(self)
+            h = trigHandler.registerDigital(self, trigLine)
+            self.triggerNow = h.triggerNow
             if 'setExposing' not in callbacks:
                 cb = lambda name, state: trigHandler.setDigital(trigLine, state)
                 callbacks['setExposing'] = cb
