1e1363465e47fa2b947f527b6bf1e1db8ef24372
Carnë Draug <carandraug+dev@gmail.com>
2018-10-26 16:39:21 +0100

    interfaces.stageMover.optimisedSiteOrder: always return a new list and not a ref
    If the new distance is longer, we return the same order as the input.  If so,
    make sure we return a copy of the list and not the same list.
    
    Also, note that on the name, i.e., use `optimised` instead of `optimise`
    (just like Python's `sorted` and `list.sort`).  Also spelling.

--- cockpit/interfaces/stageMover.py
+++ cockpit/interfaces/stageMover.py
@@ -528,7 +528,7 @@ def setSoftMax(axis, value):
 # use it if it's superior, on the assumption that users will typically
 # select sites in some basically sane order.
 # \param baseOrder List of site IDs.
-def optimizeSiteOrder(baseOrder):
+def optimisedSiteOrder(baseOrder):
     markedPoints = set()
     pointsInOrder = []
     totalTourCost = 0
@@ -566,5 +566,5 @@ def optimizeSiteOrder(baseOrder):
     if simpleTourCost < totalTourCost:
         # Nearest-neighbor is worse than just going in the
         # user-specified order
-        return baseOrder
+        return baseOrder.copy()
     return pointsInOrder
