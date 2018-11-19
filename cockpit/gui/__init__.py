3b33c76cb6c2fa022cc6168d220143262d337e2f
Carnë Draug <carandraug+dev@gmail.com>
2018-11-19 16:36:45 +0000

    cockpit.gui.EvtEmitter: new class emit wx custom events from cockpit.events
    * cockpit/gui/__init__.py: new `EvtHandler` class subscribes to
      `cockpit.events` and emits wx custom event `EVT_COCKPIT` to
      parent windows.  We need to settle on an interface, and over
      time this should be able to replace `cockpit.events` and
      `cockpit.util.threads.callInMainThread` in the whole
      `cockpit.gui` subpackage.
    
    * cockpit/testsuite/test_gui.py: tests for the new `EvtHandler`
      class and initial framework to test wx gui.

--- cockpit/gui/__init__.py
+++ cockpit/gui/__init__.py
@@ -20,6 +20,11 @@
 
 import pkg_resources
 
+import cockpit.events
+
+import wx
+
+
 ## The resource_name argument for resource_filename is not a
 ## filesystem filepath.  It is a /-separated filepath, even on
 ## windows, so do not use os.path.join.
@@ -33,3 +38,65 @@ BITMAPS_PATH = pkg_resources.resource_filename(
     'cockpit',
     'resources/bitmaps/'
 )
+
+
+## XXX: Still unsure about this design.  There's a single event type
+## for all cockpit.events which means we can't easily pass the data
+## from those events.  But having a new wx event for each of them
+## seems overkill and cause more duplication.
+EVT_COCKPIT = wx.PyEventBinder(wx.NewEventType())
+
+class CockpitEvent(wx.PyEvent):
+    def __init__(self):
+        super(CockpitEvent, self).__init__()
+        self.SetEventType(EVT_COCKPIT.typeId)
+
+
+class EvtEmitter(wx.EvtHandler):
+    """Receives :mod:`cockpit.events` and emits a custom :class:`wx.Event`.
+
+    GUI elements must beget instances of :class:`EvtEmitter` for each
+    cockpit event they are interested in subscribing, and then bind
+    whatever to :const:`EVT_COCKPIT` events.  Like so::
+
+      abort_emitter = cockpit.gui.EvtEmitter(window, cockpit.events.USER_ABORT)
+      abort_emitter.Bind(cockpit.gui.EVT_COCKPIT, window.OnUserAbort)
+
+    This ensures that cockpit events are handled in a wx compatible
+    manner.  We can't have the GUI elements subscribe directly to
+    :mod:`cockpit.events` because:
+
+    1. The function or method used for subscription needs to be called
+    on the main thread since wx, like most GUI toolkits, is not thread
+    safe.
+
+    2. unsubscribing is tricky.  wx objects are rarely destroyed so we
+    can't use the destructor.  Even :meth:`wx.Window.Destroy` is not
+    always called.
+
+    """
+    def __init__(self, parent, cockpit_event_type):
+        assert isinstance(parent, wx.Window)
+        super(EvtEmitter, self).__init__()
+        self._cockpit_event_type = cockpit_event_type
+        cockpit.events.subscribe(self._cockpit_event_type,
+                                 self._EmitCockpitEvent)
+
+        ## Destroy() is not called when the parent is destroyed, see
+        ## https://github.com/wxWidgets/Phoenix/issues/630 so we need
+        ## to handle this ourselves.
+        parent.Bind(wx.EVT_WINDOW_DESTROY, self._OnParentDestroy)
+
+    def _EmitCockpitEvent(self, *args, **kwargs):
+        self.AddPendingEvent(CockpitEvent())
+
+    def _Unsubscribe(self):
+        cockpit.events.unsubscribe(self._cockpit_event_type,
+                                   self._EmitCockpitEvent)
+
+    def _OnParentDestroy(self, event):
+        self._Unsubscribe()
+
+    def Destroy(self):
+        self._Unsubscribe()
+        return super(EventHandler, self).Destroy()
