microscope-cockpit/cockpit

commit 18baf6bc6439bbc1e4ea10c2fd74a5cf896591ee

stageMover: copy handlers to have same number of handlers per axis (issue #413)
stageMover is not great when there are different number of handlers
for each axis (e.g., one XY axis and two Z axis).  Functions need to
check a missing mover or use the closest one.  The "right" fix might
be to have each axis being selected independently (instead of a single
index that selects all movers from the three axis lists).  In the mean
time, we can hack this by making copies of the movers.

cockpit/gui/macroStage/macroStageXY.py
@@ -447,9 +447,8 @@ class MacroStageXY(macroStageBase.MacroStageBase):
             if (posDelta[axis]**2 > .001 ):
                     limits = cockpit.interfaces.stageMover.getIndividualHardLimits(axis)
                     currentpos = allPositions[currentHandlerIndex][axis]
-                    if  ((currentpos == None ) or  # no handler on this axis.
-                          (currentpos + posDelta[axis]<(limits[currentHandlerIndex][0])) or # off bottom
-                          (currentpos + posDelta[axis]>(limits[currentHandlerIndex][1]))): #off top
+                    if ((currentpos + posDelta[axis]<(limits[currentHandlerIndex][0])) # off bottom
+                        or (currentpos + posDelta[axis]>(limits[currentHandlerIndex][1]))): #off top
                         currentHandlerIndex -= 1 # go to a bigger handler index
                     if currentHandlerIndex<0:
                         return False

cockpit/interfaces/stageMover.py
@@ -137,6 +137,15 @@ class StageMover:
         ## Maps axis to the handlers for that axis, sorted by their range of
         # motion.
         self.axisToHandlers = depot.getSortedStageMovers()
+
+        ## XXX: We have a single index for all axis, even though each
+        ## axis may have a different number of stages.  While we don't
+        ## refactor this assumption, we just make copies of the movers
+        ## with the most precise movement (issues #413 and #415)
+        self.n_stages = max([len(s) for s in self.axisToHandlers.values()])
+        for axis, stages in self.axisToHandlers.items():
+            stages.extend([stages[-1]] * (self.n_stages - len(stages)))
+
         ## Indicates which stage handler is currently under control.
         self.curHandlerIndex = 0
         ## Maps Site unique IDs to Site instances.
@@ -181,21 +190,20 @@ class StageMover:
             for i, handler in enumerate(self.axisToHandlers[axis]):
                 if i != self.curHandlerIndex:
                     offset += handler.getPosition()
-            #check if we have a mover on this axis at this level
-            if(self.curHandlerIndex < len(self.axisToHandlers[axis])):
-                handler = self.axisToHandlers[axis][self.curHandlerIndex]
-                # Check if we need to bother moving.
-                if abs(handler.getPosition() - (target - offset)) > STAGE_MIN_MOVEMENT:
-                    event = threading.Event()
-                    waiters.append(event)
-                    self.nameToStoppedEvent[handler.name] = event
-                    handler.moveAbsolute(target - offset)
-                if shouldBlock:
-                    for event in waiters:
-                        try:
-                            event.wait(30)
-                        except Exception as e:
-                            print ("Failed waiting for stage to stop after 30s")
+
+            handler = self.axisToHandlers[axis][self.curHandlerIndex]
+            # Check if we need to bother moving.
+            if abs(handler.getPosition() - (target - offset)) > STAGE_MIN_MOVEMENT:
+                event = threading.Event()
+                waiters.append(event)
+                self.nameToStoppedEvent[handler.name] = event
+                handler.moveAbsolute(target - offset)
+            if shouldBlock:
+                for event in waiters:
+                    try:
+                        event.wait(30)
+                    except Exception as e:
+                        print ("Failed waiting for stage to stop after 30s")
 
 
 
@@ -244,17 +252,17 @@ def removePrimitivesByDevice(device):
 #        negative) to take along that axis.
 def step(direction):
     for axis, sign in enumerate(direction):
-        if (axis in mover.axisToHandlers and
-                mover.curHandlerIndex < len(mover.axisToHandlers[axis])):
-            #IMD 20150414 don't need to move if sign==0.
-            # Prevents aerotech axis unlocking stage on every keyboard move.
-            if (sign !=0):
-                mover.axisToHandlers[axis][mover.curHandlerIndex].moveStep(sign)
+        handler = mover.axisToHandlers[axis][mover.curHandlerIndex]
+
+        #IMD 20150414 don't need to move if sign==0.
+        # Prevents aerotech axis unlocking stage on every keyboard move.
+        if sign != 0:
+            handler.moveStep(sign)
 
 
 ## Change to the next handler.
 def changeMover():
-    newIndex = (mover.curHandlerIndex + 1) % max(map(len, mover.axisToHandlers.values()))
+    newIndex = (mover.curHandlerIndex + 1) % mover.n_stages
     if newIndex != mover.curHandlerIndex:
         mover.curHandlerIndex = newIndex
         events.publish("stage step index", mover.curHandlerIndex)
@@ -263,9 +271,9 @@ def changeMover():
 ## Change the step size for the current handlers.
 def changeStepSize(direction):
     for axis, handlers in iteritems(mover.axisToHandlers):
-        if mover.curHandlerIndex < len(handlers):
-            handlers[mover.curHandlerIndex].changeStepSize(direction)
-            events.publish("stage step size", axis, handlers[mover.curHandlerIndex].getStepSize())
+        handlers[mover.curHandlerIndex].changeStepSize(direction)
+        events.publish("stage step size", axis,
+                       handlers[mover.curHandlerIndex].getStepSize())
 
 
 ## Recenter the fine-motion devices by adjusting the large-scale motion
@@ -419,13 +427,11 @@ def getPositionForAxis(axis):
 # then those axes will have None instead of a position towards the
 # end of the list.
 def getAllPositions():
-    mostMovers = max(map(len, mover.axisToHandlers.values()))
     result = []
-    for i in range(mostMovers):
-        current = [None for axis in range(len(mover.axisToHandlers.keys()))]
-        for axis, handlers in iteritems(mover.axisToHandlers):
-            if i < len(handlers):
-                current[axis] = handlers[i].getPosition()
+    for i in range(mover.n_stages):
+        current = [None] * len(mover.axisToHandlers)
+        for axis, handlers in mover.axisToHandlers.items():
+            current[axis] = handlers[i].getPosition()
         result.append(tuple(current))
     return result
 
@@ -436,11 +442,7 @@ def getAllPositions():
 def getCurStepSizes():
     result = [None] * len(mover.axisToHandlers)
     for axis, handlers in iteritems(mover.axisToHandlers):
-        if mover.curHandlerIndex < len(handlers):
-            step_size = handlers[mover.curHandlerIndex].getStepSize()
-        else:
-            step_size = None
-        result[axis] = step_size
+        result[axis] = handlers[mover.curHandlerIndex].getStepSize()
     return tuple(result)
 
 

commit feb72cadc6db6ba39008c921a15dd5839c4bd5ce

stageMover: use copies and unique handlers for each axis (issue #413)
This reverts commit bb2233975620 (which had reverted 18baf6bc6439) and
fixes the issues that caused it to be reverted.

Commit 18baf6b broke the mosaic and macrostage window.  This was
because moves and sizes are computed taking into account all
position and limits of all handlers and now we have copies of
the same handlers.  This ensures that only unique handlers are
used for such computations.

cockpit/gui/macroStage/macroStageXY.py
@@ -447,9 +447,8 @@ class MacroStageXY(macroStageBase.MacroStageBase):
             if (posDelta[axis]**2 > .001 ):
                     limits = cockpit.interfaces.stageMover.getIndividualHardLimits(axis)
                     currentpos = allPositions[currentHandlerIndex][axis]
-                    if  ((currentpos == None ) or  # no handler on this axis.
-                          (currentpos + posDelta[axis]<(limits[currentHandlerIndex][0])) or # off bottom
-                          (currentpos + posDelta[axis]>(limits[currentHandlerIndex][1]))): #off top
+                    if ((currentpos + posDelta[axis]<(limits[currentHandlerIndex][0])) # off bottom
+                        or (currentpos + posDelta[axis]>(limits[currentHandlerIndex][1]))): #off top
                         currentHandlerIndex -= 1 # go to a bigger handler index
                     if currentHandlerIndex<0:
                         return False

cockpit/interfaces/stageMover.py
@@ -137,6 +137,15 @@ class StageMover:
         ## Maps axis to the handlers for that axis, sorted by their range of
         # motion.
         self.axisToHandlers = depot.getSortedStageMovers()
+
+        ## XXX: We have a single index for all axis, even though each
+        ## axis may have a different number of stages.  While we don't
+        ## refactor this assumption, we just make copies of the movers
+        ## with the most precise movement (issues #413 and #415)
+        self.n_stages = max([len(s) for s in self.axisToHandlers.values()])
+        for axis, stages in self.axisToHandlers.items():
+            stages.extend([stages[-1]] * (self.n_stages - len(stages)))
+
         ## Indicates which stage handler is currently under control.
         self.curHandlerIndex = 0
         ## Maps Site unique IDs to Site instances.
@@ -178,24 +187,23 @@ class StageMover:
         for axis, target in position:
             # Get the offset for the movers that aren't being adjusted.
             offset = 0
-            for i, handler in enumerate(self.axisToHandlers[axis]):
-                if i != self.curHandlerIndex:
+            for handler in self.axisToHandlers[axis]:
+                if handler != self.axisToHandlers[axis][self.curHandlerIndex]:
                     offset += handler.getPosition()
-            #check if we have a mover on this axis at this level
-            if(self.curHandlerIndex < len(self.axisToHandlers[axis])):
-                handler = self.axisToHandlers[axis][self.curHandlerIndex]
-                # Check if we need to bother moving.
-                if abs(handler.getPosition() - (target - offset)) > STAGE_MIN_MOVEMENT:
-                    event = threading.Event()
-                    waiters.append(event)
-                    self.nameToStoppedEvent[handler.name] = event
-                    handler.moveAbsolute(target - offset)
-                if shouldBlock:
-                    for event in waiters:
-                        try:
-                            event.wait(30)
-                        except Exception as e:
-                            print ("Failed waiting for stage to stop after 30s")
+
+            handler = self.axisToHandlers[axis][self.curHandlerIndex]
+            # Check if we need to bother moving.
+            if abs(handler.getPosition() - (target - offset)) > STAGE_MIN_MOVEMENT:
+                event = threading.Event()
+                waiters.append(event)
+                self.nameToStoppedEvent[handler.name] = event
+                handler.moveAbsolute(target - offset)
+            if shouldBlock:
+                for event in waiters:
+                    try:
+                        event.wait(30)
+                    except Exception as e:
+                        print ("Failed waiting for stage to stop after 30s")
 
 
 
@@ -244,17 +252,17 @@ def removePrimitivesByDevice(device):
 #        negative) to take along that axis.
 def step(direction):
     for axis, sign in enumerate(direction):
-        if (axis in mover.axisToHandlers and
-                mover.curHandlerIndex < len(mover.axisToHandlers[axis])):
-            #IMD 20150414 don't need to move if sign==0.
-            # Prevents aerotech axis unlocking stage on every keyboard move.
-            if (sign !=0):
-                mover.axisToHandlers[axis][mover.curHandlerIndex].moveStep(sign)
+        handler = mover.axisToHandlers[axis][mover.curHandlerIndex]
+
+        #IMD 20150414 don't need to move if sign==0.
+        # Prevents aerotech axis unlocking stage on every keyboard move.
+        if sign != 0:
+            handler.moveStep(sign)
 
 
 ## Change to the next handler.
 def changeMover():
-    newIndex = (mover.curHandlerIndex + 1) % max(map(len, mover.axisToHandlers.values()))
+    newIndex = (mover.curHandlerIndex + 1) % mover.n_stages
     if newIndex != mover.curHandlerIndex:
         mover.curHandlerIndex = newIndex
         events.publish("stage step index", mover.curHandlerIndex)
@@ -263,9 +271,9 @@ def changeMover():
 ## Change the step size for the current handlers.
 def changeStepSize(direction):
     for axis, handlers in iteritems(mover.axisToHandlers):
-        if mover.curHandlerIndex < len(handlers):
-            handlers[mover.curHandlerIndex].changeStepSize(direction)
-            events.publish("stage step size", axis, handlers[mover.curHandlerIndex].getStepSize())
+        handlers[mover.curHandlerIndex].changeStepSize(direction)
+        events.publish("stage step size", axis,
+                       handlers[mover.curHandlerIndex].getStepSize())
 
 
 ## Recenter the fine-motion devices by adjusting the large-scale motion
@@ -401,7 +409,7 @@ def loadSites(filename):
 def getPosition():
     result = 3 * [0]
     for axis, handlers in iteritems(mover.axisToHandlers):
-        for handler in handlers:
+        for handler in set(handlers):
             result[axis] += handler.getPosition()
     return result
 
@@ -409,7 +417,7 @@ def getPosition():
 ## Return the exact stage position for the given axis.
 def getPositionForAxis(axis):
     result = 0
-    for handler in mover.axisToHandlers[axis]:
+    for handler in set(mover.axisToHandlers[axis]):
         result += handler.getPosition()
     return result
 
@@ -419,13 +427,11 @@ def getPositionForAxis(axis):
 # then those axes will have None instead of a position towards the
 # end of the list.
 def getAllPositions():
-    mostMovers = max(map(len, mover.axisToHandlers.values()))
     result = []
-    for i in range(mostMovers):
-        current = [None for axis in range(len(mover.axisToHandlers.keys()))]
-        for axis, handlers in iteritems(mover.axisToHandlers):
-            if i < len(handlers):
-                current[axis] = handlers[i].getPosition()
+    for i in range(mover.n_stages):
+        current = [None] * len(mover.axisToHandlers)
+        for axis, handlers in mover.axisToHandlers.items():
+            current[axis] = handlers[i].getPosition()
         result.append(tuple(current))
     return result
 
@@ -436,11 +442,7 @@ def getAllPositions():
 def getCurStepSizes():
     result = [None] * len(mover.axisToHandlers)
     for axis, handlers in iteritems(mover.axisToHandlers):
-        if mover.curHandlerIndex < len(handlers):
-            step_size = handlers[mover.curHandlerIndex].getStepSize()
-        else:
-            step_size = None
-        result[axis] = step_size
+        result[axis] = handlers[mover.curHandlerIndex].getStepSize()
     return tuple(result)
 
 
@@ -454,7 +456,7 @@ def getCurHandlerIndex():
 def getHardLimitsForAxis(axis):
     lowLimit = 0
     highLimit = 0
-    for handler in mover.axisToHandlers[axis]:
+    for handler in set(mover.axisToHandlers[axis]):
         low, high = handler.getHardLimits()
         lowLimit += low
         highLimit += high
@@ -479,7 +481,7 @@ def getIndividualHardLimits(axis):
 def getSoftLimitsForAxis(axis):
     lowLimit = 0
     highLimit = 0
-    for handler in mover.axisToHandlers[axis]:
+    for handler in set(mover.axisToHandlers[axis]):
         low, high = handler.getSoftLimits()
         lowLimit += low
         highLimit += high