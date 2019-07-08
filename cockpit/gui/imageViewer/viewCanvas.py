3febff096a758d799a7bd8527f9e9512878f9a75
Nicholas Hall <lina2938@bioch.ox.ac.uk>
2019-07-08 17:17:31 +0100

    Only show deltas when there are two centroids shown

--- cockpit/gui/imageViewer/viewCanvas.py
+++ cockpit/gui/imageViewer/viewCanvas.py
@@ -648,10 +648,15 @@ class ViewCanvas(wx.glcanvas.GLCanvas):
             glOrtho (0, self.w, 0, self.h, 1., -1.)
             glTranslatef(0, HISTOGRAM_HEIGHT/2+2, 0)
             try:
-                self.font.render('%d [%-10d %10d] %d    X diff = %.5f, Y diff = %.5f' %
+                if self.showAligCentroid and self.showCurCentroid:
+                    self.font.render('%d [%-10d %10d] %d    X diff = %.4f, Y diff = %.4f' %
                                 (self.image.dmin, self.histogram.lthresh,
                                 self.histogram.uthresh, self.image.dmin + self.image.dptp,
                                 self.diff_x, self.diff_y))
+                else:
+                    self.font.render('%d [%-10d %10d] %d' %
+                                     (self.image.dmin, self.histogram.lthresh,
+                                      self.histogram.uthresh, self.image.dmin + self.image.dptp))
             except:
                 pass
             glPopMatrix()
