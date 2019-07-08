0076024b7637259b44eae2227a62f948f3e2d207
Mick Phillips <mick.phillips@gmail.com>
2019-07-08 10:34:04 +0100

    Fix setup.py to work with README, not README.md.
    The README files were renamed in
    9b794aaa0f8d6c2297637b8ecbabc31dff27461f.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools.command.sdist
 ## distribution.  We could also have a MANIFEST file but we'd rather
 ## not have the distribution configuration over multiple files.
 manifest_files = [
-    'README.md',
+    'README',
     'COPYING',
     os.path.join('cockpit', 'resources', 'fonts', 'Universalis_COPYING.txt'),
     os.path.join('cockpit', 'resources', 'fonts', 'Universalis_NOTICE.txt'),
@@ -33,7 +33,7 @@ setuptools.setup(
     name = 'cockpit',
     version = '2.9.0+dev',
     description = 'Hardware agnostic microscope user interface',
-    long_description = open('README.md', 'r').read(),
+    long_description = open('README', 'r').read(),
     license = 'GPL-3.0+',
 
     url = "https://github.com/MicronOxford/cockpit",
