993616fdfa21f7d4dd3bfc59d85e02381a43dacf
NickHallONBI <nicholas.hall@dtc.ox.ac.uk>
2019-10-23 11:24:47 +0100

    Added more metric options

--- cockpit/devices/microscopeDeformableMirror.py
+++ cockpit/devices/microscopeDeformableMirror.py
@@ -98,7 +98,10 @@ class MicroscopeDeformableMirror(MicroscopeBase, device.Device):
         # A mapping of context-menu entries to functions.
         # Define in tuples - easier to read and reorder.
         menuTuples = (('Fourier metric', 'fourier'),
-                      ('Contrast metric', 'contrast'),)
+                      ('Contrast metric', 'contrast'),
+                      ('Fourier Power metric', 'fourier_power'),
+                      ('Gradient metric', 'gradient'),
+                      ('Second Moment metric', 'second_moment'),)
         # Store as ordered dict for easy item->func lookup.
         self.menuItems = OrderedDict(menuTuples)
 
