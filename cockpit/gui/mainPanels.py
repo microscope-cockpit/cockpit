94a1d4a12f1ddd58ca90b28b517abe155e408964
David Miguel Susano Pinto <carandraug+dev@gmail.com>
2021-03-08 19:09:39 +0000

    mainPanels: do not hardcode height of coloured line under camera and lights.
    The lines under each camera and light shows their corresponding
    wavelengths and have a fixed height of 4 pixels.  This commit makes
    that height function of the font to better fit the system settings.

--- cockpit/gui/mainPanels.py
+++ cockpit/gui/mainPanels.py
@@ -52,7 +52,8 @@ class LightPanel(wx.Panel):
 
         self.Sizer.Add(self.button, flag=wx.EXPAND)
         self.Sizer.AddSpacer(2)
-        line = wx.Control(self, size=(-1,4))
+        line_height = int(self.GetFont().GetFractionalPointSize() / 2.0)
+        line = wx.Control(self, size=(-1, line_height))
         line.SetBackgroundColour(wavelengthToColor(self.light.wavelength))
         self.Sizer.Add(line, flag=wx.EXPAND)
 
@@ -130,7 +131,8 @@ class CameraPanel(wx.Panel):
         self.Sizer.Add(self.button, flag=wx.EXPAND)
         self.Sizer.AddSpacer(2)
 
-        self.line = wx.Control(self, size=(-1,4))
+        line_height = int(self.GetFont().GetFractionalPointSize() / 2.0)
+        self.line = wx.Control(self, size=(-1, line_height))
         self.line.SetBackgroundColour(wavelengthToColor(self.camera.wavelength or 0))
         self.Sizer.Add(self.line, flag=wx.EXPAND)
         # If there are problems here, it's because the inline function below is
