microscope-cockpit/cockpit

commit 7f8ab9ffd4d9d6a1899cf09a7fc8838730578432

Experiment.is_running: method to query instance (issue #433)
There is a module function to know if there is an experiment running
but there was no way to know if a specific Experiment instance was
running.  This adds the method.

cockpit/experiment/experiment.py
@@ -80,13 +80,10 @@ lastExperiment = None
 # multiple files.
 generatedFilenames = []
 
-_runThread = None
-
 def isRunning():
-    if _runThread is None:
-        return False
-    else:
-        return _runThread.is_alive()
+    """Is an experiment running?
+    """
+    return lastExperiment.is_running()
 
 
 ## This class is the root class for generating and running experiments.
@@ -134,6 +131,9 @@ class Experiment:
         self.otherHandlers = list(otherHandlers)
         self.metadata = metadata
         self.savePath = savePath
+
+        self._run_thread = None
+
         # Check for save paths that don't actually have a final filename
         # (i.e. just point to a directory); those aren't valid.
         if not os.path.basename(self.savePath):
@@ -229,9 +229,9 @@ class Experiment:
         # display appropriate warnings.
         self.lastMinuteActions()
 
-        runThread = threading.Thread(target=self.execute, name="Experiment-execute")
-        global _runThread
-        _runThread = runThread
+        self._run_thread = threading.Thread(target=self.execute,
+                                            name="Experiment-execute")
+        self._run_thread.start()
 
         saveThread = None
         if self.savePath and max(self.cameraToImageCount.values()):
@@ -257,17 +257,19 @@ class Experiment:
             saver = dataSaver.DataSaver(self.cameras, self.numReps,
                                         self.cameraToImageCount,
                                         self.cameraToIgnoredImageIndices,
-                                        runThread, self.savePath,
+                                        self._run_thread, self.savePath,
                                         self.sliceHeight, self.generateTitles(),
                                         cameraToExcitation)
             saver.startCollecting()
-            saveThread = threading.Thread(target=saver.executeAndSave, name="Experiment-execute-save")
+            saveThread = threading.Thread(target=saver.executeAndSave,
+                                          name="Experiment-execute-save")
             saveThread.start()
             generatedFilenames.append(saver.getFilenames())
 
-        runThread.start()
-        # Start up a thread to clean up after the experiment finishes.
-        threading.Thread(target=self.cleanup, args=[runThread, saveThread], name="Experiment-cleanup").start()
+        cleanup_thread = threading.Thread(target=self.cleanup,
+                                          args=[self._run_thread, saveThread],
+                                          name="Experiment-cleanup")
+        cleanup_thread.start()
         return True
 
     ## Create an ActionTable by calling self.generateActions, and give our
@@ -684,6 +686,15 @@ class Experiment:
             if camera in cameras and lightTimePairs:
                 exposureTime = max(exposureTime, max(lightTimePairs, key = lambda a: a[1])[1])
         return exposureTime
+
+    def is_running(self):
+        """Is this experiment running?
+        """
+        if self._run_thread is None:
+            return False
+        else:
+            return self._run_thread.is_alive()
+
 
 ## Return a list of the files generated by the most recent experiment.
 def getLastFilenames():

commit 9a24d62e99945b1189e8a88ca6490c5dabb5185d

Experiment.run: start run thread after starting the saver thread (#734)
If the run thread (Experiment.execute) starts before the saver thread
(DataSaver.executeAndSave) and DataSaver.startCollecting, then images
acquired between those get lost.

This commit moves the start of the run thread until after the save
saver is ready.  This bug is a regression introduced with 7f8ab9ffd4.

cockpit/experiment/experiment.py
@@ -234,7 +234,6 @@ class Experiment:
 
         self._run_thread = threading.Thread(target=self.execute,
                                             name="Experiment-execute")
-        self._run_thread.start()
 
         saveThread = None
         if self.savePath and max(self.cameraToImageCount.values()):
@@ -268,6 +267,8 @@ class Experiment:
                                           name="Experiment-execute-save")
             saveThread.start()
             generatedFilenames.append(saver.getFilenames())
+
+        self._run_thread.start()
 
         cleanup_thread = threading.Thread(target=self.cleanup,
                                           args=[self._run_thread, saveThread],