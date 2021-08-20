9e5ce75236b1563cff67df3675d2ebc9a552caa0
David Miguel Susano Pinto <carandraug+dev@gmail.com>
2021-08-20 15:55:36 +0100

    cockpit/gui/touchscreen.py: use ## in copyright notice for consistency.
    All other files in the project use double # in the copyright notice
    except touchscreen.py.  This commit changes that.  The consistency
    helps in writing scripts to query or change this section.

--- cockpit/gui/touchscreen.py
+++ cockpit/gui/touchscreen.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2018-2019 Mick Phillips <mick.phillips@gmail.com>
-# Copyright (C) 2018 Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
-# Copyright (C) 2021 Danail Stoychev <danail.stoychev@exeter.ox.ac.uk>
-#
-# This file is part of Cockpit.
-#
-# Cockpit is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Cockpit is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Cockpit.  If not, see <http://www.gnu.org/licenses/>.
+## Copyright (C) 2018-2019 Mick Phillips <mick.phillips@gmail.com>
+## Copyright (C) 2018 Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
+## Copyright (C) 2021 Danail Stoychev <danail.stoychev@exeter.ox.ac.uk>
+##
+## This file is part of Cockpit.
+##
+## Cockpit is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Cockpit is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Cockpit.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
