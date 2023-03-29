eb36be277ca6503c91c606385d45382eab1be924
Thomas Fish <50103643+thomasmfish@users.noreply.github.com>
2023-03-29 14:47:22 +0100

    Potential exit crash fix (#5)
    * potential exit crash fix?
    
    * remove accidental loop

--- cockpit/gui/mainWindow.py
+++ cockpit/gui/mainWindow.py
@@ -540,15 +540,21 @@ class MainWindow(wx.Frame):
         objects have been destroyed already.
         """
         if not event.CanVeto():
-            event.Destroy()
+            MainWindow.destroyAllWindows()
         else:
             wx.GetApp()._SaveWindowPositions()
+            MainWindow.destroyAllWindows()
             # Let the default event handler handle the frame
             # destruction.
             event.Skip()
 
     def _OnAbout(self, event):
         wx.adv.AboutBox(CockpitAboutInfo(), parent=self)
+
+    @staticmethod
+    def destroyAllWindows():
+        for w in wx.GetTopLevelWindows():
+            wx.CallAfter(w.Destroy)
 
 
 class StatusLights(wx.StatusBar):
