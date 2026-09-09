95ead9e7a866759b8b919735c73b8321fc3f5bce
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2026-09-09 10:26:44 +0200

    Added PI-M687 stage configuration options to swap and invert axes

--- cockpit/devices/physikInstrumenteM687.py
+++ cockpit/devices/physikInstrumenteM687.py
@@ -118,12 +118,18 @@ class PhysikInstrumenteM687(Device):
         baud: 115200
         timeout: 0.1
         softlimits: ((-37500,-67500),(11500,59500))
+        swapaxes: 1
+        invert0: 0
+        invert1: 0
 
     """
 
     _config_types = {
         "baud": int,
         "timeout": float,
+        "swapaxes": int,
+        "invert0": int,
+        "invert1": int,
     }
 
     def __init__(self, name, config):
@@ -139,11 +145,19 @@ class PhysikInstrumenteM687(Device):
         # not moving.
         self.xyMotionTargets = [None, None]
         ## Maps the cockpit's axis ordering (0: X, 1: Y, 2: Z) to the
-        # XY stage's ordering (1: Y, 2: X)
-        self.axisMapper = {0: 2, 1: 1}
+        # XY stage's ordering or swap it if True in the configuration
+        if self.config.get("swapaxes", False):
+            self.axisMapper = {0: 2, 1: 1}
+        else:
+            self.axisMapper = {0: 1, 1: 2}
         ## Maps cockpit axis ordering to a +-1 multiplier to apply to motion,
-        # since some of our axes are flipped.
-        self.axisSignMapper = {0: -1, 1: 1}
+        # since some of our axes might be flipped.
+        invert_0 = config.get("invert0", False)
+        invert_1 = config.get("invert1", False)
+        self.axisSignMapper = {
+            0: -1 if invert_0 else 1,
+            1: -1 if invert_1 else 1,
+        }
 
         ## If there is a config section for the m687, grab the config and
         # subscribe to events.
