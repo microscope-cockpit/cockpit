25847b5bf45231dc81eb99b357c1f94bc7a7a9af
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2026-09-09 12:09:56 +0200

    Fixed error in the logic for swapping axes

--- cockpit/devices/physikInstrumenteM687.py
+++ cockpit/devices/physikInstrumenteM687.py
@@ -147,9 +147,9 @@ class PhysikInstrumenteM687(Device):
         ## Maps the cockpit's axis ordering (0: X, 1: Y, 2: Z) to the
         # XY stage's ordering or swap it if True in the configuration
         if self.config.get("swapaxes", False):
-            self.axisMapper = {0: 2, 1: 1}
-        else:
             self.axisMapper = {0: 1, 1: 2}
+        else:
+            self.axisMapper = {0: 2, 1: 1}
         ## Maps cockpit axis ordering to a +-1 multiplier to apply to motion,
         # since some of our axes might be flipped.
         invert_0 = config.get("invert0", False)
